9c8950d34aee89144041a778efc6fbdbcb730933
bdbrandt <ben@greentheorystudio.com>
2020-08-06 09:25:37 -0700

    Legacy file work

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 	<tr>
 		<td class="footer">
             <div style="display:flex;justify-content:center;flex-wrap:wrap;margin-top:8px;margin-bottom:8px;">
-                <img src="images/layout/oliveline2.jpg" style="width:90%;height:15px;" />
+                <img src="<?php echo $CLIENT_ROOT; ?>/images/layout/oliveline2.jpg" style="width:90%;height:15px;" />
             </div>
             <div style="width:100%;z-index:10;margin-bottom:10px;">
                 <div style="width:100%;color:black;opacity:1;display:flex;justify-content:space-evenly;align-items:center;padding:15px;">
