5a780332600449bd35170eba86a5a005d1d15ae0
bdbrandt <ben@greentheorystudio.com>
2021-01-07 15:50:25 -0700

    Changes to homepage and styling per request

--- index.php
+++ index.php
@@ -17,7 +17,31 @@ header('Content-Type: text/html; charset=' .$CHARSET);
         include(__DIR__ . '/header.php');
         ?>
         <div  id="innertext">
-            <div style="padding: 0 15px;">
+            <div style="float:right;width:380px;">
+                <?php
+                $oodID = 1;
+                $ootdGameChecklist = 1;
+                $ootdGameTitle = 'Plant of the Day ';
+                $ootdGameType = 'plant';
+
+                include_once(__DIR__ . '/classes/GamesManager.php');
+                $gameManager = new GamesManager();
+                $gameInfo = $gameManager->setOOTD($oodID,$ootdGameChecklist);
+                ?>
+                <div style="float:right;margin-top:15px;margin-right:10px;margin-bottom:15px;width:350px;text-align:center;">
+                    <div style="font-size:130%;font-weight:bold;">
+                        <?php echo $ootdGameTitle; ?>
+                    </div>
+                    <a href="<?php echo $CLIENT_ROOT; ?>/games/ootd/index.php?oodid=<?php echo $oodID.'&cl='.$ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
+                        <img src="<?php echo $CLIENT_ROOT; ?>/temp/ootd/<?php echo $oodID; ?>_organism300_1.jpg" style="width:350px;border:0;" />
+                    </a><br/>
+                    <b>What is this <?php echo $ootdGameType; ?>?</b><br/>
+                    <a href="<?php echo $CLIENT_ROOT; ?>/games/ootd/index.php?oodid=<?php echo $oodID.'&cl='.$ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
+                        Click here to test your knowledge
+                    </a>
+                </div>
+            </div>
+            <div style="padding: 0 15px;margin-top:15px;">
                 The genus <i>Lomatium</i> is the largest of 19 genera of Apiaceae from primarily Western North America that
                 form a large clade of over 200 species.  This website is the result of an NSF funded project to create a
                 phylogenetic hypothesis and a new classification for this previously poorly understood group.  This electronic
@@ -26,10 +50,26 @@ header('Content-Type: text/html; charset=' .$CHARSET);
                 also included.
             </div>
             <div style="margin-top:15px;padding: 0 10px;">
-                The 19 genera covered in this treatment are: <i>Aletes</i>, <i>Cymopterus</i>, <i>Eurytaenia</i>, <i>Harbouria</i>,
-                <i>Lomatium</i>, <i>Musineon</i>, <i>Neoparrya</i>, <i>Oreonana</i>, <i>Oreoxis</i>, <i>Podistera</i>, <i>Polytaenia</i>,
-                <i>Pseudocymopterus</i>, <i>Pteryxia</i>, <i>Shoshonea</i>, <i>Taenidia</i>, <i>Tauschia</i>, <i>Thaspium</i>,
-                <i>Vesper</i>, and <i>Zizia</i>.
+                The 19 genera covered in this treatment are:
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Aletes" ><i>Aletes</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Cymopterus" ><i>Cymopterus</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Eurytaenia" ><i>Eurytaenia</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Harbouria" ><i>Harbouria</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Lomatium" ><i>Lomatium</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Musineon" ><i>Musineon</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Neoparrya" ><i>Neoparrya</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Oreonana" ><i>Oreonana</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Oreoxis" ><i>Oreoxis</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Podistera" ><i>Podistera</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Polytaenia" ><i>Polytaenia</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Pseudocymopterus" ><i>Pseudocymopterus</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Pteryxia" ><i>Pteryxia</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Shoshonea" ><i>Shoshonea</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Taenidia" ><i>Taenidia</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Tauschia" ><i>Tauschia</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Thaspium" ><i>Thaspium</i></a>,
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Vesper" ><i>Vesper</i></a>, and
+                <a href="<?php echo $CLIENT_ROOT; ?>/taxa/index.php?taxon=Zizia" ><i>Zizia</i></a>.
             </div>
             <div style="margin: 25px auto 0;width:475px;">
                 <div style="clear:both;padding:5px;" >
