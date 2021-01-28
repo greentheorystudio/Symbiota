c297b7ae7da6713044a9da93d96e37211708e132
bdbrandt <ben@greentheorystudio.com>
2021-01-28 12:32:08 -0700

    Taxa profile page

---
+++ taxa/includes/config/taxaProfileElementsCustom.php
@@ -0,0 +1,43 @@
+<?php
+ob_start();
+echo '<div id="footerlinkstoggle">';
+if($taxonRank > 180 && $links) {
+    echo '<a href="#" onclick="toggleLinks(\'links\');return false;">Web Links</a>';
+}
+
+$starrValue = '{"usethes":true,"taxontype":"1","taxa":"'.$taxonManager->getSciName().'"}';
+echo "<div style='margin-top:15px;text-align:center;nowrap;'>";
+echo '<form name="occurrences" id="occurrences" action="../collections/list.php" method="post">';
+echo "<input type='hidden' name='starr' value='".$starrValue."' />";
+echo '<input type="hidden" name="page" value="1" />';
+echo '</form>';
+
+if($taxonRank > 140){
+    $parentLink = 'index.php?taxon='.$taxonManager->getParentTid().'&taxauthid='.$taxAuthId;
+    if($clValue) {
+        $parentLink .= "&cl=" . $taxonManager->getClid();
+    }
+    if($projValue) {
+        $parentLink .= "&proj=" . $projValue;
+    }
+    $taxonTerm = ((int)$taxonRank === 220?"Genus":"Family");
+    echo "&nbsp;&nbsp;&nbsp;<a href='".$parentLink."'>View ".($taxonRank > 220?"Parent":$taxonTerm)."</a>&nbsp;&nbsp;&nbsp;&nbsp;";
+    echo '&nbsp;&nbsp;&nbsp;<a href="#" onclick="document.getElementById(\'occurrences\').submit();">View Specimen Records</a>&nbsp;&nbsp;&nbsp;&nbsp;';
+}
+if($taxonRank > 180 && $taxonManager->getNumChildren() > 0 && !$displayingChildren) {
+    echo '&nbsp;&nbsp;&nbsp;<a href="index.php?taxon='.$taxonManager->getTid().'&displaychildren=1">View Subspecies/Varieties</a>&nbsp;&nbsp;&nbsp;&nbsp;';
+}
+echo '</div>';
+
+if($taxonRank > 180 && $links){
+    echo '<div id="links" style="display:none;"><h1 id="linksbanner">Web Links</h1><ul id="linkslist">';
+    foreach($links as $l){
+        $urlStr = str_replace('--SCINAME--',rawurlencode($taxonManager->getSciName()),$l['url']);
+        echo '<li><a href="'.$urlStr.'" target="_blank">'.$l['title'].'</a></li>';
+        if($l['notes']) {
+            echo ' ' . $l['notes'];
+        }
+    }
+    echo "</ul>\n</div>";
+}
+$wisFloraFooterLinksDiv = ob_get_clean();
---
+++ taxa/includes/config/taxaProfileTemplateCustom.php
@@ -0,0 +1,44 @@
+<?php
+$displayingChildren = array_key_exists('displaychildren',$_REQUEST)?$_REQUEST['displaychildren']: 0;
+
+include('includes/config/taxaProfileElementsDefault.php');
+if(file_exists('includes/config/taxaProfileElementsCustom.php')){
+    include('includes/config/taxaProfileElementsCustom.php');
+}
+
+$topRowElements = Array();
+$leftColumnElements = Array();
+$rightColumnElements = Array();
+$bottomRowElements = Array();
+$footerRowElements = Array();
+
+if($taxonRank){
+    if($taxonRank > 180 && !$displayingChildren){
+        $topRowElements = Array($editButtonDiv,$scinameHeaderDiv,$ambiguousDiv,$webLinksDiv);
+        $leftColumnElements = Array($familyDiv,$vernacularsDiv,$synonymsDiv,$centralImageDiv);
+        $rightColumnElements = Array($descTabsDiv);
+        $bottomRowElements = Array($mapThumbDiv,$imgDiv,$imgTabDiv);
+        $footerRowElements = Array($wisFloraFooterLinksDiv);
+    }
+    elseif($taxonRank === 180 || $displayingChildren){
+        $topRowElements = Array();
+        $leftColumnElements = Array($scinameHeaderDiv,$familyDiv,$projectDiv,$centralImageDiv);
+        $rightColumnElements = Array($editButtonDiv,$descTabsDiv);
+        $bottomRowElements = Array($imgBoxDiv);
+        $footerRowElements = Array($wisFloraFooterLinksDiv);
+    }
+    else{
+        $topRowElements = Array();
+        $leftColumnElements = Array($scinameHeaderDiv,$familyDiv,$projectDiv,$centralImageDiv);
+        $rightColumnElements = Array($editButtonDiv,$descTabsDiv);
+        $bottomRowElements = Array($imgBoxDiv);
+        $footerRowElements = Array($wisFloraFooterLinksDiv);
+    }
+}
+elseif($taxonValue){
+    $topRowElements = Array($notFoundDiv);
+}
+else{
+    $topRowElements = Array('ERROR!');
+}
+?>
