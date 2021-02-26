f8d0c0100d7df8876be95229c4a741204b87820a
bdbrandt <ben@greentheorystudio.com>
2021-02-26 10:32:02 -0700

    Misc repairs

--- classes/SpecUploadBase.php
+++ classes/SpecUploadBase.php
@@ -1133,29 +1133,27 @@ class SpecUploadBase extends SpecUpload{
 
                 $sqlFragments = $this->getSqlFragments($recMap,$this->identFieldMap);
                 if($sqlFragments){
-                    if(array_key_exists('identifiedby',$recMap) || array_key_exists('dateidentified',$recMap)){
-                        if(!array_key_exists('identifiedby',$recMap)) {
-                            $recMap['identifiedby'] = 'not specified';
-                        }
-                        if(!array_key_exists('dateidentified',$recMap)) {
-                            $recMap['dateidentified'] = 'not specified';
-                        }
-                        $sql = 'INSERT INTO uploaddetermtemp(collid'.$sqlFragments['fieldstr'].') '.
-                            'VALUES('.$this->collId.$sqlFragments['valuestr'].')';
-                        //echo "<div>SQL: ".$sql."</div>"; exit;
+                    if(!array_key_exists('identifiedby',$recMap)) {
+                        $recMap['identifiedby'] = 'not specified';
+                    }
+                    if(!array_key_exists('dateidentified',$recMap)) {
+                        $recMap['dateidentified'] = 'not specified';
+                    }
+                    $sql = 'INSERT INTO uploaddetermtemp(collid'.$sqlFragments['fieldstr'].') '.
+                        'VALUES('.$this->collId.$sqlFragments['valuestr'].')';
+                    //echo "<div>SQL: ".$sql."</div>"; exit;
 
-                        if($this->conn->query($sql)){
-                            $this->identTransferCount++;
-                            if($this->identTransferCount%1000 === 0) {
-                                $this->outputMsg('<li style="margin-left:10px;">Count: ' . $this->identTransferCount . '</li>');
-                            }
+                    if($this->conn->query($sql)){
+                        $this->identTransferCount++;
+                        if($this->identTransferCount%1000 === 0) {
+                            $this->outputMsg('<li style="margin-left:10px;">Count: ' . $this->identTransferCount . '</li>');
                         }
-                        else{
-                            $outStr = '<li>FAILED adding identification history record #'.$this->identTransferCount.'</li>';
-                            $outStr .= '<li style="margin-left:10px;">Error: '.$this->conn->error.'</li>';
-                            $outStr .= '<li style="margin:0 0 10px 10px;">SQL: '.$sql.'</li>';
-                            $this->outputMsg($outStr);
-                        }
+                    }
+                    else{
+                        $outStr = '<li>FAILED adding identification history record #'.$this->identTransferCount.'</li>';
+                        $outStr .= '<li style="margin-left:10px;">Error: '.$this->conn->error.'</li>';
+                        $outStr .= '<li style="margin:0 0 10px 10px;">SQL: '.$sql.'</li>';
+                        $this->outputMsg($outStr);
                     }
                 }
             }
