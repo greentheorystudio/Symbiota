f523af480e176034f37c5070815b726b1c2cdd0b
bdbrandt <ben@greentheorystudio.com>
2021-04-09 09:36:44 -0700

    Initial commit

---
+++ header.php
@@ -0,0 +1,104 @@
+<table id="maintable" style="border-spacing: 0;">
+    <tr>
+        <td class="header">
+            <div style="clear:both;">
+                <div style="float:left;">
+                    <img style="" src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/images/layout/Picture1.png" />
+                </div>
+                <div style="float:left;">
+                    <img style="" src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/images/layout/title.png" />
+                </div>
+            </div>
+            <div id="top_navbar">
+                <div id="right_navbarlinks">
+                    <?php
+                    if($GLOBALS['USER_DISPLAY_NAME']){
+                        ?>
+                        <span style="">
+							Welcome <?php echo $GLOBALS['USER_DISPLAY_NAME']; ?>!
+						</span>
+                        <span style="margin-left:5px;">
+							<a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/profile/viewprofile.php">My Profile</a>
+						</span>
+                        <span style="margin-left:5px;">
+							<a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/profile/index.php?submit=logout">Logout</a>
+						</span>
+                        <?php
+                    }
+                    else{
+                        ?>
+                        <span style="">
+							<a href="<?php echo $GLOBALS['CLIENT_ROOT']. '/profile/index.php?refurl=' .$_SERVER['PHP_SELF']. '?' .$_SERVER['QUERY_STRING']; ?>">
+								Log In
+							</a>
+						</span>
+                        <span style="margin-left:5px;">
+							<a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/profile/newprofile.php">
+								New Account
+							</a>
+						</span>
+                        <?php
+                    }
+                    ?>
+                    <span style="margin-left:5px;margin-right:5px;">
+						<a href='<?php echo $GLOBALS['CLIENT_ROOT']; ?>/sitemap.php'>Sitemap</a>
+					</span>
+
+                </div>
+                <ul id="hor_dropdown">
+                    <li>
+                        <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/index.php" >Home</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/collections/index.php" >Search Collections</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/spatial/index.php" target="_blank">Spatial Module</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/imagelib/search.php" >Image Search</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/imagelib/index.php" >Browse Images</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/projects/index.php?" >Inventories</a>
+                        <!-- <ul>
+							<li>
+								<a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/projects/index.php?pid=1" >Project 1</a>
+							</li>
+							<li>
+								<a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/projects/index.php?pid=2" >Project 2</a>
+							</li>
+							<li>
+								<a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/projects/index.php?pid=3" >Project 3</a>
+							</li>
+							<li>
+								<a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/projects/index.php?pid=4" >Project 4</a>
+							</li>
+						</ul> -->
+                    </li>
+                    <li>
+                        <a href="#" >Interactive Tools</a>
+                        <!-- <ul>
+							<li>
+								<a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/checklists/dynamicmap.php?interface=checklist&tid=1" >Dynamic Checklist 1</a>
+							</li>
+							<li>
+								<a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/checklists/dynamicmap.php?interface=checklist&tid=2" >Dynamic Checklist 2</a>
+							</li>
+							<li>
+								<a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/checklists/dynamicmap.php?interface=checklist&tid=3" >Dynamic Checklist 3</a>
+							</li>
+							<li>
+								<a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/checklists/dynamicmap.php?interface=checklist&tid=4" >Dynamic Checklist 4</a>
+							</li>
+						</ul> -->
+                    </li>
+                </ul>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <td class="middlecenter">
+
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+include_once("config/symbini.php");
+header("Content-Type: text/html; charset=".$CHARSET);
+?>
+<html>
+<head>
+    <meta http-equiv="X-Frame-Options" content="deny">
+    <title><?php echo $DEFAULT_TITLE; ?> Home</title>
+    <link href="css/base.css?ver=<?php echo $CSS_VERSION; ?>" type="text/css" rel="stylesheet" />
+    <link href="css/main.css<?php echo (isset($CSS_VERSION_LOCAL)?'?ver='.$CSS_VERSION_LOCAL:''); ?>" type="text/css" rel="stylesheet" />
+    <script type="text/javascript">
+        <?php include_once($SERVER_ROOT.'/config/googleanalytics.php'); ?>
+    </script>
+</head>
+<body>
+<?php
+include($SERVER_ROOT.'/header.php');
+?>
+<!-- This is inner text! -->
+<div  id="innertext">
+    <h1>Welcome to SeedTrack</h1>
+
+    <div style="padding: 0px 10px;">
+        The Symbiota software system has developed a library of webtools to aid biologists in establishing specimen-based
+        biological research and can be extended to the seed banking plant conservation community and expand its number of users.
+        Symbiota is a data access technology connecting biodiversity collections around the world and serves as a data management
+        system primarily for in-situ natural history specimen collections. The new SeedTrack module proposed here will be an
+        ex-situ data management extension and a new feature of the open access Symbiota software package. Some of the data
+        structure and its multi-user functionality currently exists within the Symbiota software as Darwin Core terms. The Darwin
+        Core is a predefined subset of the terms that have common use across a wide variety of biodiversity applications.
+        The terms used in the Simple Darwin Core are those that are found at the cross-section of taxonomic names, places,
+        and events that document biological specimen occurrences on the planet. The two driving principles are simplicity
+        and flexibility.
+    </div>
+
+    <div style="margin-top:10px;padding: 0px 10px;">
+        The module will be developed using existing functionality within Symbiota to track ex-situ plant and seed collections,
+        and add further functionality to track plant propagation methods used in growing events and link those events to
+        specimen based collections. The new data module will have the ability to be visible to users who wish to incorporate
+        the new plant conservation feature.
+    </div>
+    <div style="margin-top:10px;padding: 0px 10px;">
+        The restoration module will be developed as an extension to the Symbiota biological informatics system to:
+        <ol start="1" type="1">
+            <li>Provide users (plant conservationists, seed collectors, data managers, curators, and researchers) with
+                a core data standard to track propagule specimen collections (primarily seed) and seedlings in both
+                cultivated and wild populations.
+            </li>
+            <li>Standardize data to assess the seed viability in disjunct species populations.</li>
+            <li>Track and share specimen collection events, propagation methodology, source native species plant material,
+                and document out-planted seedlings.</li>
+            <li>Allow for the tracking of changes that occur during the propagation and growing cycle such as changes
+                in fertilizer regime, changes in pot size, and plant mortality rate.</li>
+        </ol>
+    </div>
+</div>
+
+<?php
+include($SERVER_ROOT.'/footer.php');
+?>
+</body>
+</html>
