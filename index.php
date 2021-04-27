5797d6ca4ca4e9008ca8fa1fa71853961a9e82e0
bdbrandt <ben@greentheorystudio.com>
2021-04-27 09:50:04 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+include_once(__DIR__ . '/config/symbini.php');
+header('Content-Type: text/html; charset=' .$GLOBALS['CHARSET']);
+?>
+<html lang="<?php echo $GLOBALS['DEFAULT_LANG']; ?>">
+    <head>
+        <title><?php echo $GLOBALS['DEFAULT_TITLE']; ?> Home</title>
+        <link href="css/base.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" type="text/css" rel="stylesheet" />
+        <link href="css/main.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" type="text/css" rel="stylesheet" />
+        <meta name='keywords' content='' />
+        <?php include_once(__DIR__ . '/config/googleanalytics.php'); ?>
+    </head>
+    <body>
+        <?php
+        include(__DIR__ . '/header.php');
+        ?>
+        <div  id="innertext">
+            <h1></h1>
+
+            <div style="padding: 0 10px;">
+                Description and introduction of project
+            </div>
+        </div>
+
+        <?php
+        include(__DIR__ . '/footer.php');
+        ?>
+    </body>
+</html>
