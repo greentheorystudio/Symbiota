10a1fd1009becbb69769537e3fa7fb8646fd85c0
bdbrandt <ben@greentheorystudio.com>
2021-05-29 08:11:53 -0700

    Added credit to footer

--- footer.php
+++ footer.php
@@ -1,13 +1,15 @@
-    	</td>
-	</tr>
-	<tr>
-		<td class="footer">
-			<div style="display:flex;justify-content:space-around;margin-bottom:10px;">
-                <div style="">
-                    <div style="font-size:20px;font-family:Arial, Helvetica, sans-serif;margin-bottom:8px;">Brought to you by </div>
-                    <img style="width:475px;" src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/images/layout/gts_logo_light_white_70_lg.png" />
-                </div>
+</td>
+</tr>
+<tr>
+    <td class="footer">
+        <div style="display:flex;justify-content:space-around;margin-bottom:10px;">
+            <div style="">
+                <div style="font-size:16px;font-family:Verdana, sans-serif;margin-bottom:8px;">Brought to you by </div>
+                <a href="https://greentheorystudio.com/" target="_blank">
+                    <img style="width:400px;" src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/images/layout/gts_logo_light_white_70_lg.png" />
+                </a>
             </div>
-		</td>
-	</tr>
+        </div>
+    </td>
+</tr>
 </table>
