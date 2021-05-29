10d9be2ba244b6dddaf879535987fc15733babf3
bdbrandt <ben@greentheorystudio.com>
2021-05-28 23:47:38 -0700

    Added credit to footer

--- footer.php
+++ footer.php
@@ -2,7 +2,12 @@
 	</tr>
 	<tr>
 		<td class="footer">
-			
+			<div style="display:flex;justify-content:space-around;margin-bottom:10px;">
+                <div style="">
+                    <div style="font-size:20px;font-family:Arial, Helvetica, sans-serif;margin-bottom:8px;">Brought to you by </div>
+                    <img style="width:475px;" src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/images/layout/gts_logo_light_white_70_lg.png" />
+                </div>
+            </div>
 		</td>
 	</tr>
 </table>
