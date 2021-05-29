956b0fcfedc1a47ee15e9412a5fd31d0f740191a
bdbrandt <ben@greentheorystudio.com>
2021-05-29 08:12:46 -0700

    Added credit to footer

--- footer.php
+++ footer.php
@@ -1,18 +1,15 @@
 </td>
 </tr>
 <tr>
-	<td class="footer" colspan="3">
-		<div style="font-size:14px;width:650px;height:100px;margin-left:auto;margin-right:auto;margin-bottom:15px;display:table;" >
-			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/NSF_web_transperent.gif" width="100" height="100" alt="The National Science Foundation" />
-			<div style="padding-left:10px;display:table-cell;vertical-align: middle;">
-				Development of <a href="http://symbiota.org" target="_blank">Symbiota</a>, and several of the specimen databases have been supported by
-				National Science Foundation Grants
-				(<a href="http://nsf.gov/awardsearch/showAward.do?AwardNumber=9983132" target="_blank">DBI 9983132</a>,
-				<a href="http://nsf.gov/awardsearch/showAward.do?AwardNumber=0237418" target="_blank">BRC 0237418</a>,
-				<a href="http://www.nsf.gov/awardsearch/showAward.do?AwardNumber=0743827" target="_blank">DBI 0743827</a>,
-				<a href="http://www.nsf.gov/awardsearch/showAward.do?AwardNumber=0847966" target="_blank">DBI 0847966</a>)
-			</div>
-		</div>
-	</td>
+    <td class="footer">
+        <div style="display:flex;justify-content:space-around;margin-bottom:10px;">
+            <div style="">
+                <div style="font-size:16px;font-family:Verdana, sans-serif;margin-bottom:8px;">Brought to you by </div>
+                <a href="https://greentheorystudio.com/" target="_blank">
+                    <img style="width:400px;" src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/images/layout/gts_logo_light_white_70_lg.png" />
+                </a>
+            </div>
+        </div>
+    </td>
 </tr>
 </table>
