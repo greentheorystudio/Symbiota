1360e88d5f9ef2d924a0103d9391f3c4e35a0b3f
bdbrandt <ben@greentheorystudio.com>
2021-07-25 14:24:41 -0700

    Initial commit updates

--- index.php
+++ index.php
@@ -3,27 +3,22 @@ include_once(__DIR__ . '/config/symbini.php');
 header('Content-Type: text/html; charset=' .$GLOBALS['CHARSET']);
 ?>
 <html lang="<?php echo $GLOBALS['DEFAULT_LANG']; ?>">
-    <head>
-        <title><?php echo $GLOBALS['DEFAULT_TITLE']; ?> Home</title>
-        <link href="css/base.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" type="text/css" rel="stylesheet" />
-        <link href="css/main.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" type="text/css" rel="stylesheet" />
-        <meta name='keywords' content='' />
-        <?php include_once(__DIR__ . '/config/googleanalytics.php'); ?>
-    </head>
-    <body>
-        <?php
-        include(__DIR__ . '/header.php');
-        ?>
-        <div  id="innertext">
-            <h1></h1>
+<head>
+    <title><?php echo $GLOBALS['DEFAULT_TITLE']; ?> Home</title>
+    <link href="css/base.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" type="text/css" rel="stylesheet" />
+    <link href="css/main.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" type="text/css" rel="stylesheet" />
+    <meta name='keywords' content='' />
+    <?php include_once(__DIR__ . '/config/googleanalytics.php'); ?>
+</head>
+<body>
+<?php
+include(__DIR__ . '/header.php');
+?>
+<div id="innertext">
+</div>
 
-            <div style="padding: 0 10px;">
-                Description and introduction of project
-            </div>
-        </div>
-
-        <?php
-        include(__DIR__ . '/footer.php');
-        ?>
-    </body>
+<?php
+include(__DIR__ . '/footer.php');
+?>
+</body>
 </html>
