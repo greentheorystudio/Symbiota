f8e14e7f93933428c97bb6f49d8da0a0066d6ad5
bdbrandt <ben@greentheorystudio.com>
2022-02-08 12:20:48 -0700

    Version update

--- collections/admin/specifyupdater.php
+++ collections/admin/specifyupdater.php
@@ -1,5 +1,5 @@
 <?php
-include_once(__DIR__ . '/../../config/symbini.php');
+include_once(__DIR__ . '/../../config/symbbase.php');
 include_once(__DIR__ . '/../../classes/SpecifyUpdateManager.php');
 include_once(__DIR__ . '/../../classes/SpecifyDwcArchiverOccurrence.php');
 include_once(__DIR__ . '/../../classes/SpecUploadBase.php');
