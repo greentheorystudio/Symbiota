cca9191bb006cc662124b7407ad2e87ecc1a8eb2
bdbrandt <ben@greentheorystudio.com>
2022-05-18 18:06:02 -0700

    Added validated user attribute to current user global attributes

--- config/symbbase.php
+++ config/symbbase.php
@@ -6,7 +6,7 @@ include_once(__DIR__ . '/../classes/Sanitizer.php');
 Sanitizer::validateRequestPath();
 ini_set('session.gc_maxlifetime',3600);
 ini_set('session.cookie_httponly',1);
-if((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') || (isset($_SERVER['SERVER_PORT']) && (int) $_SERVER['SERVER_PORT'] === 443)){
+if((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') || (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] === 443)){
     ini_set('session.cookie_secure',1);
 }
 session_start();
