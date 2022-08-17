f72dbdd9ba221fdf2f412935cd2704e1976a91d3
bdbrandt <ben@greentheorystudio.com>
2022-08-17 10:10:24 -0700

    Tutorial WIP

--- tutorial/spatial/index.php
+++ tutorial/spatial/index.php
@@ -344,47 +344,82 @@ header('Content-Type: text/html; charset=' .$GLOBALS['CHARSET']);
                             and <i style="height:20px;width:20px;" class="fas fa-border-all"></i> for raster data.</p>
                     </div>
                     <div class="tutorial-frame fragment fade-in-then-out" style="width: 25%; right: 5%; top: 25%;">
-                        <p>Each layer is depicted in a box within the Layers Panel. In the top-left of the box is the layer name,
-                            which corresponds with the layer name in the <a href="index.php#/control-panel/3">Active Layer Selector</a> if
-                            the layer is visible. Any metadata that has been configured with layer, including description, who
-                            provided the layer, source link, date acquired, and date uploaded, will appear below the layer name. In
-                            the bottom-left corner of the box will be an icon to indicate whether the layer contains vector or raster
-                            data, with <i style="height:20px;width:20px;" class="fas fa-vector-square"></i> for vector data
-                            and <i style="height:20px;width:20px;" class="fas fa-border-all"></i> for raster data.</p>
+                        <p>Each layer's information and controls are enclosed within individual frames within the Layers Panel. In
+                            the top-left of each frame is the layer name, which corresponds with the layer name in
+                            the <a href="index.php#/control-panel/3">Active Layer Selector</a>, if the layer is visible. Any metadata
+                            that has been configured with the layer, including description, who provided the layer, source link, date
+                            acquired, and date uploaded, will appear below the layer name. In the bottom-left corner of each
+                            frame will be an icon to indicate whether the layer contains vector or raster data,
+                            with <i style="height:20px;width:20px;" class="fas fa-vector-square"></i> for vector data and <i style="height:20px;width:20px;" class="fas fa-border-all"></i> for
+                            raster data.</p>
                     </div>
                     <div class="tutorial-frame fragment fade-in-then-out" style="width: 25%; right: 5%; top: 25%;">
-                        <p>In the far bottom-right corner of each layer box in the Layers Panel is the checkbox to toggle whether
-                            the layer is visible on the map. If this box is checked, and the layer is visible, other controls
-                            will also be availble to the left of this checkbox. Checking this checkbox will show the layer on the
+                        <p>The layer controls are found in the bottom-right of each layer frame. If the layer is currently not
+                            visible, the controls may be limited to one or two, but more will be available once the layer's
+                            visibility is changed. We will go through each control that is available, what layers it is available
+                            for, and how to use each. We will start with the control in the far bottom-right corner of each layer
+                            frame and address each control to the left:</p>
+                    </div>
+                    <div class="tutorial-frame fragment fade-in-then-out" style="width: 25%; right: 5%; top: 25%;">
+                        <h3>Visibility Toggle</h3>
+                        <p><b>Available for every layer, both when visible and hidden.</b> In the far bottom-right corner of each layer frame is the Visibility Toggle checkbox,
+                            which toggles whether the layer is visible on the map. Checking this checkbox will show the layer on the
                             map, and unchecking it will hide the layer. It is important to note that this checkbox simply
                             toggles the visibility of a layer and does not affect the layer data.</p>
                     </div>
                     <div class="tutorial-frame fragment fade-in-then-out" style="width: 25%; right: 5%; top: 25%;">
-                        <h3>Cluster Points</h3>
-                        <p>When checked (default) occrrence points loaded onto the map are clustered, based on the cluster
-                            distance setting, into clusters of like records. When not checked, all occurrence records are loaded
-                            as individual points on the map, regardless of proximity.</p>
+                        <h3>Delete Layer Button</h3>
+                        <p><b>Available for the Shapes and Points layers and any layers that you have loaded onto the map, both
+                            when visible and hidden.</b> Clicking this button will completely remove the layer, and all of its data,
+                            from the map. If this layer is the Shapes layer, this means that all features included in that layer
+                            will be deleted. If this layer is the Points layer, this means that all search results will be
+                            removed from the map and the Records Panel. Clicing this button cannot be undone.</p>
                     </div>
                     <div class="tutorial-frame fragment fade-in-then-out" style="width: 25%; right: 5%; top: 25%;">
-                        <h3>Cluster Distance</h3>
-                        <p>This setting controls the minimum distance threshold (in pixels) between points or clusters which
-                            determine whether they are clustered when Cluster Points is checked.</p>
+                        <h3>Query Selector Toggle Button</h3>
+                        <p><b>Available for vector layers when visible.</b> Clicking this will close the Layers Panel and open
+                            the Query Selector for the layer. The Query Selector allows you to select features within a given
+                            layer based on attribute values and will be discussed further in a later topic.</p>
                     </div>
                     <div class="tutorial-frame fragment fade-in-then-out" style="width: 25%; right: 5%; top: 25%;">
-                        <h3>Display Heat Map</h3>
-                        <p>When checked occrrence points loaded onto the map are displayed as a heat map, based on the Heat Map
-                            Radius and Heat Map Blur settings. When not checked (default) occurrence points are displayed on
-                            the map as individual points, or clusters of like points.</p>
+                        <h3>Symbology Toggle Button</h3>
+                        <p><b>Available for all layers, except the Shapes and Points Layers, when visible.</b> Clicking this
+                            button will open and close the layer's symbology settings, where you can adjust how the layer is
+                            symbolized on the map. For vector layers, these settings include: border color, fill color, border
+                            width, point radius (for point features), and opacity. To adjust border color and fill color, simply
+                            click on the color indicator box next to each, and then select the color you would like from the
+                            color picker that opens. For raster layers, you can select one of the several predefined color scales
+                            for symbolizing raster data. All symbology adjustments made for any layer are immediately reflected
+                            on the map.</p>
                     </div>
                     <div class="tutorial-frame fragment fade-in-then-out" style="width: 25%; right: 5%; top: 25%;">
-                        <h3>Heat Map Radius</h3>
-                        <p>This setting controls the radius (in pixels) of occurrence points in the heat map display when Display
-                            Heat Map is checked.</p>
+                        <h3>Order Adjuster</h3>
+                        <p><b>Available for all layers, except the Shapes and Points Layers, when visible.</b> This will indicate,
+                            and allow you to adjust, the order in which the layer is located within the stack of layers currently
+                            visible on the map. As layers are added to the map, they are added over previously added layers and
+                            have incrementally higher order numbers in the stack of layers. Layers with lower order numbers apeear
+                            underneath layers of higher order numbers on the map. You can use this adjuster to either move a layer
+                            higher or lower within the layer stack, and therefore make it appear above or below other layers
+                            on the map.</p>
+                    </div>
+                </section>
+                <section id="query-selector" data-background-iframe="../../spatial/index.php" data-background-interactive data-preload>
+                    <div style="position:absolute;left: 50%; bottom:10;">
+                        <div class="tutorial-frame" style="position:relative; left: -50%;">
+                            <div class="slide-title">Query Selector</div>
+                            <div class="index-link"><a href="index.php#/index">Back to index</a></div>
+                        </div>
                     </div>
                     <div class="tutorial-frame fragment fade-in-then-out" style="width: 25%; right: 5%; top: 25%;">
-                        <h3>Heat Map Blur</h3>
-                        <p>This setting controls the blur size (in pixels) of points in the heat map display when Display
-                            Heat Map is checked.</p>
+                        <p>Within the Query Selector, the Select attribute drop-down contains
+                            all of the attributes applied to features within that layer. Select the attribute you would like to
+                            use to base your query. Then select the operator in the next drop-down, out of EQUALS, CONTAINS,
+                            GREATER THAN, LESS THAN, and BETWEEN. Both the EQUALS and CONTAINS operators can compare text or
+                            numeric values, but the remaining operators can only compare numeric values. Then enter a value in
+                            the next box, or value range if the BETWEEN operator is selected. Then click the Run Query button
+                            to actually run the query. This will select all features within the layer that match the criteria
+                            you have specified and automatically add them to the Shapes Layer (to be discussed further in the
+                            next topic).</p>
                     </div>
                 </section>
             </div>
