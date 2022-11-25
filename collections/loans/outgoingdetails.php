af3296722882bab2133b87248b87e7b6e6e3e46f
bdbrandt <ben@greentheorystudio.com>
2022-11-25 13:59:47 -0700

    Occurrence Taxonomy manager development

--- collections/loans/outgoingdetails.php
+++ collections/loans/outgoingdetails.php
@@ -7,7 +7,7 @@
 $specList = $loanManager->getSpecList($loanId);
 ?>
 <script type="text/javascript" src="../../js/shared.js?ver=20221121"></script>
-<script type="text/javascript" src="../../js/collections.occureditormain.js?ver=20221115"></script>
+<script type="text/javascript" src="../../js/collections.occureditormain.js?ver=20221125"></script>
 <div id="tabs" style="margin:0;">
     <ul>
 		<li><a href="#outloandetaildiv"><span>Loan Details</span></a></li>
