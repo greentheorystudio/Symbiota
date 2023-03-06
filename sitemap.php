1e0104e20bded369b76512168974ccdecdbad835
bdbrandt <ben@greentheorystudio.com>
2023-03-06 16:39:41 -0700

    WIP

--- sitemap.php
+++ sitemap.php
@@ -128,7 +128,7 @@ $smManager = new SiteMapManager();
                             if($GLOBALS['IS_ADMIN'] || array_key_exists('TaxonProfile',$GLOBALS['USER_RIGHTS'])){
                                 ?>
                                 <li><a href="taxa/profile/tpeditor.php">Taxon Profile Manager</a></li>
-                                <li><a href="taxa/media/index.php">Taxa Media Batch Uploader</a></li>
+                                <li><a href="taxa/media/batchimageloader.php">Taxa Media Batch Uploader</a></li>
                                 <?php
                             }
                             ?>
