5fc25da788d460bbbc34f9c60c9089b29ece5983
bdbrandt <ben@greentheorystudio.com>
2023-06-23 13:55:41 -0700

    Misc repairs

--- classes/ConfigurationManager.php
+++ classes/ConfigurationManager.php
@@ -148,7 +148,7 @@ class ConfigurationManager{
         if(!isset($GLOBALS['DEFAULT_TITLE'])){
             $GLOBALS['DEFAULT_TITLE'] = '';
         }
-        $GLOBALS['CSS_VERSION'] = '20230619';
+        $GLOBALS['CSS_VERSION'] = '20230621';
         $GLOBALS['PARAMS_ARR'] = array();
         $GLOBALS['USER_RIGHTS'] = array();
         $this->validateGlobalArr();
