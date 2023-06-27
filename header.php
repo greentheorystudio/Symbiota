627656e64604ab7f703f4dcc819c1ef9ee35c121
bdbrandt <ben@greentheorystudio.com>
2023-06-27 09:24:41 -0700

    Update

--- header.php
+++ header.php
@@ -15,10 +15,10 @@ include_once(__DIR__ . '/classes/Sanitizer.php');
                         <q-list dense>
                             <template v-for="item in navBarData">
                                 <template v-if="item.subItems && item.subItems.length">
-                                    <q-item clickable v-close-popup :href="item.url" :target="(item.newTab?'_blank':'_self')" v-model="navBarToggle[item.id]" @mouseover="navBarToggle[item.id] = true" @mouseleave="navBarToggle[item.id] = false">
+                                    <q-item clickable v-close-popup :href="item.url" :target="(item.newTab?'_blank':'_self')" v-model="navBarToggle[item.id]" @mouseover="navbarToggleOn(item.id)" @mouseleave="navbarToggleOff(item.id)">
                                         <q-item-section>{{ item.label }}</q-item-section>
-                                        <q-menu v-model="navBarToggle[item.id]" transition-duration="750" anchor="top end" self="top start">
-                                            <q-list dense @mouseover="navBarToggle[item.id] = true" @mouseleave="navBarToggle[item.id] = false">
+                                        <q-menu v-model="navBarToggle[item.id]" transition-duration="100" anchor="top end" self="top start">
+                                            <q-list dense @mouseover="navbarToggleOn(item.id)" @mouseleave="navbarToggleOff(item.id)">
                                                 <template v-for="subitem in item.subItems">
                                                     <q-item clickable v-close-popup :href="subitem.url" :target="(subitem.newTab?'_blank':'_self')">
                                                         <q-item-section>{{ subitem.label }}</q-item-section>
@@ -41,9 +41,9 @@ include_once(__DIR__ . '/classes/Sanitizer.php');
             <template v-if="windowWidth >= 1440">
                 <template v-for="item in navBarData">
                     <template v-if="item.subItems && item.subItems.length">
-                        <q-btn class="horizontalDropDownButton text-capitalize" :href="item.url" :target="(item.newTab?'_blank':'_self')" :label="item.label" v-model="navBarToggle[item.id]" @mouseover="navBarToggle[item.id] = true" @mouseleave="navBarToggle[item.id] = false" stretch flat no-wrap>
-                            <q-menu v-model="navBarToggle[item.id]" transition-duration="750" anchor="bottom start" self="top start" square>
-                                <q-list dense @mouseover="navBarToggle[item.id] = true" @mouseleave="navBarToggle[item.id] = false">
+                        <q-btn class="horizontalDropDownButton text-capitalize" :href="item.url" :target="(item.newTab?'_blank':'_self')" :label="item.label" v-model="navBarToggle[item.id]" @mouseover="navbarToggleOn(item.id)" @mouseleave="navbarToggleOff(item.id)" stretch flat no-wrap>
+                            <q-menu v-model="navBarToggle[item.id]" transition-duration="100" anchor="bottom start" self="top start" square>
+                                <q-list dense @mouseover="navbarToggleOn(item.id)" @mouseleave="navbarToggleOff(item.id)">
                                     <template v-for="subitem in item.subItems">
                                         <q-item class="horizontalDropDownButton text-capitalize" :href="subitem.url" :target="(subitem.newTab?'_blank':'_self')" clickable v-close-popup>
                                             <q-item-section>
@@ -64,7 +64,7 @@ include_once(__DIR__ . '/classes/Sanitizer.php');
             <template v-if="userDisplayName">
                 <q-breadcrumbs-el class="header-username-text">Welcome {{ userDisplayName }}!</q-breadcrumbs-el>
                 <q-btn class="horizontalDropDownButton text-capitalize" href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/profile/viewprofile.php" label="My Profile" stretch flat no-wrap></q-btn>
-                <q-btn class="horizontalDropDownButton text-capitalize" href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/profile/index.php?submit=logout" label="Logout" stretch flat no-wrap></q-btn>
+                <q-btn class="horizontalDropDownButton text-capitalize" @click="logout();" label="Logout" stretch flat no-wrap></q-btn>
             </template>
             <template v-else>
                 <q-btn class="horizontalDropDownButton text-capitalize" href="<?php echo $GLOBALS['CLIENT_ROOT']. '/profile/index.php?refurl=' .Sanitizer::getCleanedRequestPath(true); ?>" label="Log In" stretch flat no-wrap></q-btn>
@@ -105,6 +105,30 @@ include_once(__DIR__ . '/classes/Sanitizer.php');
                     this.handleResize();
                 },
                 methods: {
+                    handleResize() {
+                        this.windowWidth = window.innerWidth;
+                    },
+                    logout() {
+                        const url = profileApiUrl + '?action=logout';
+                        fetch(url)
+                            .then(() => {
+                                window.location.href = CLIENT_ROOT + '/index.php';
+                            })
+                    },
+                    navbarToggleOff(id) {
+                        this.navBarTimeout = setTimeout(() => {
+                            this.navBarToggle[Number(id)] = false;
+                        }, 400);
+                    },
+                    navbarToggleOn(id) {
+                        clearTimeout(this.navBarTimeout);
+                        for(let i in this.navBarToggle){
+                            if(this.navBarToggle.hasOwnProperty(i) && Number(i) !== Number(id)){
+                                this.navBarToggle[Number(i)] = false;
+                            }
+                        }
+                        this.navBarToggle[Number(id)] = true;
+                    },
                     setNavBarData() {
                         let indexId = 1;
                         this.navBarData.forEach((dataObj) => {
@@ -114,9 +138,6 @@ include_once(__DIR__ . '/classes/Sanitizer.php');
                                 indexId++;
                             }
                         });
-                    },
-                    handleResize() {
-                        this.windowWidth = window.innerWidth;
                     }
                 }
             });
