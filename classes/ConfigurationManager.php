c3210b6d8584ebe0a9611142016ed913ff771174
bdbrandt <ben@greentheorystudio.com>
2023-07-19 14:09:42 -0700

    Display fix for taxon profile pages

--- classes/ConfigurationManager.php
+++ classes/ConfigurationManager.php
@@ -148,7 +148,7 @@ class ConfigurationManager{
         if(!isset($GLOBALS['DEFAULT_TITLE'])){
             $GLOBALS['DEFAULT_TITLE'] = '';
         }
-        $GLOBALS['CSS_VERSION'] = '20230630';
+        $GLOBALS['CSS_VERSION'] = '20230715';
         $GLOBALS['PARAMS_ARR'] = array();
         $GLOBALS['USER_RIGHTS'] = array();
         $this->validateGlobalArr();
