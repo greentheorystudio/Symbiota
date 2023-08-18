a2cf24855d45f5ff51fe8e527f0c74f34e1d435b
bdbrandt <ben@greentheorystudio.com>
2023-08-17 20:01:47 -0700

    Taxon Profile page improvement

--- taxa/profile-default.php
+++ taxa/profile-default.php
@@ -5,7 +5,7 @@
 <script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/components/taxonomy/taxaProfileTaxonFamily.js" type="text/javascript"></script>
 <script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/components/taxonomy/taxaProfileTaxonVernaculars.js?ver=20230630" type="text/javascript"></script>
 <script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/components/taxonomy/taxaProfileTaxonSynonyms.js?ver=20230630" type="text/javascript"></script>
-<script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/components/taxonomy/taxaProfileCentralmage.js?ver=20230715" type="text/javascript"></script>
+<script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/components/taxonomy/taxaProfileCentralmage.js?ver=20230817" type="text/javascript"></script>
 <script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/components/taxonomy/taxaProfileDescriptionTabs.js" type="text/javascript"></script>
 <script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/components/taxonomy/taxaProfileTaxonMap.js?ver=20230817" type="text/javascript"></script>
 <script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/components/taxonomy/taxaProfileTaxonImageLink.js?ver=20230817" type="text/javascript"></script>
@@ -36,7 +36,7 @@
                     </div>
                     <div class="profile-split-row">
                         <div class="left-column profile-column">
-                            <taxa-profile-central-image :taxon="taxon" :central-image="centralImage" :is-editor="isEditor"></taxa-profile-central-image>
+                            <taxa-profile-central-image :taxon="taxon" :central-image="centralImage" :is-editor="isEditor" @update:set-image-carousel="showImageCarousel"></taxa-profile-central-image>
                         </div>
                         <div class="right-column profile-column">
                             <taxa-profile-description-tabs :description-arr="descriptionArr" :glossary-arr="glossaryArr"></taxa-profile-description-tabs>
@@ -167,7 +167,7 @@
                         image['anchorUrl'] = CLIENT_ROOT + '/imagelib/imgdetails.php?imgid=' + image['id'];
                     }
                 });
-                this.centralImage = this.taxon['images'].shift();
+                this.centralImage = this.taxon['images'].length > 0 ? this.taxon['images'][0] : null;
                 if(Number(this.taxon['imageCnt']) > 100){
                     this.imageExpansionLabel = 'View First 100 Images';
                 }
