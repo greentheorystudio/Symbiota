9c6027df1da9f9153bc3dec93e5ab6fbba937546
Ben Brandt <benjamin.brandt@nau.edu>
2024-05-30 10:49:28 -0700

    Taxon profile repair

--- classes/IRLManager.php
+++ classes/IRLManager.php
@@ -60,17 +60,107 @@ class IRLManager {
     {
         $returnArr = array();
         if(file_exists($GLOBALS['SERVER_ROOT'] . '/content/json/collection' . $collid . 'occurrencedatafields.json')) {
-            $retArr = json_decode(file_get_contents($GLOBALS['SERVER_ROOT'].'/content/json/collection' . $collid . 'occurrencedatafields.json'), true);
+            $returnArr = json_decode(file_get_contents($GLOBALS['SERVER_ROOT'].'/content/json/collection' . $collid . 'occurrencedatafields.json'), true);
         }
+        return $returnArr;
+    }
+
+    public function getProjectAmbiInfaunaData($collid): array
+    {
+        $returnArr = array();
+        $alphaArr = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l');
+        $headerCodeArr = array('');
+        $headerRepArr = array('');
+        $keyArr = array();
+        $taxaDataArr = array();
+        $taxaNameArr = array();
+        $sql = 'SELECT DISTINCT l.locationCode, o.`year`, o.`month`, o.rep '.
+            'FROM omoccurrences AS o LEFT JOIN omoccurlocations AS l ON o.locationID = l.locationID '.
+            'WHERE o.collid = ' . (int)$collid . ' ORDER BY l.locationCode, o.`year`, o.`month`, o.rep ';
+        //echo $sql;
+        $result = $this->conn->query($sql);
+        while($row = $result->fetch_object()){
+            $headerCode = $row->locationCode . '.' . $row->year . '.' . ((int)$row->month < 10 ? ('0' . $row->month) : $row->month);
+            $keyCode = $headerCode . '-' . $row->rep;
+            $keyArr[] = $keyCode;
+            $headerCodeArr[] = $headerCode;
+            $headerRepArr[] = $alphaArr[((int)$row->rep - 1)];
+        }
+        $result->free();
+
+        $returnArr[] = $headerCodeArr;
+        $returnArr[] = $headerRepArr;
+
+        $sql = 'SELECT l.locationCode, o.sciname, o.`year`, o.`month`, o.rep, o.individualCount '.
+            'FROM omoccurrences AS o LEFT JOIN omoccurlocations AS l ON o.locationID = l.locationID '.
+            'WHERE o.collid = ' . (int)$collid . ' ORDER BY o.sciname ';
+        //echo $sql;
+        $result = $this->conn->query($sql);
+        while($row = $result->fetch_object()){
+            $keyCode = $row->locationCode . '.' . $row->year . '.' . ((int)$row->month < 10 ? ('0' . $row->month) : $row->month) . '-' . $row->rep;
+            if(!in_array($row->sciname, $taxaNameArr)){
+                $taxaNameArr[] = $row->sciname;
+            }
+            $taxaDataArr[$row->sciname][$keyCode] = $row->individualCount;
+        }
+        $result->free();
+
+        foreach($taxaNameArr as $sciname){
+            $rowArr = array();
+            $rowArr[] = $sciname;
+            foreach($keyArr as $key){
+                $rowArr[] = ((array_key_exists($key, $taxaDataArr[$sciname]) && (int)$taxaDataArr[$sciname][$key] > 0) ? $taxaDataArr[$sciname][$key] : '0');
+            }
+            $returnArr[] = $rowArr;
+        }
+
         return $returnArr;
     }
 
     public function getProjectEnvironmentalData($collid): array
     {
         $returnArr = array();
+        $headerArr = array('sample_date', 'site', 'time', 'depth', 'decimalLong', 'decimalLat');
+        $configuredDataArr = array();
         $configuredData = $this->getConfiguredData($collid);
-        if($configuredData && array_key_exists('dataFields', $configuredData) ){
+        if($configuredData && array_key_exists('dataFields', $configuredData)){
+            foreach($configuredData['dataFields'] as $field => $fieldArr){
+                $headerArr[] = $field;
+            }
+            $returnArr[] = $headerArr;
 
+            $sql = 'SELECT a.eventID, a.field, a.datavalue '.
+                'FROM omoccuradditionaldata AS a LEFT JOIN omoccurcollectingevents AS c ON a.eventID = c.eventID '.
+                'WHERE c.collid = ' . (int)$collid . ' ';
+            //echo $sql;
+            $result = $this->conn->query($sql);
+            while($row = $result->fetch_object()){
+                $configuredDataArr[$row->eventID][$row->field] = $row->datavalue;
+            }
+            $result->free();
+
+            $sql = 'SELECT c.eventID, c.eventDate, l.locationCode, c.eventTime, '.
+                'c.minimumDepthInMeters, IFNULL(c.decimallongitude, l.decimalLongitude) AS "decimalLong", '.
+                'IFNULL(c.decimalLatitude, l.decimalLatitude) AS "decimalLat" '.
+                'FROM omoccurcollectingevents AS c LEFT JOIN omoccurlocations AS l ON c.locationID = l.locationID '.
+                'WHERE c.collid = ' . (int)$collid . ' ORDER BY c.eventDate ';
+            //echo $sql;
+            $result = $this->conn->query($sql);
+            while($row = $result->fetch_object()){
+                $rowArr = array();
+                $eventId = $row->eventID;
+                $rowArr[] = $row->eventDate;
+                $rowArr[] = $row->locationCode;
+                $rowArr[] = $row->eventTime;
+                $rowArr[] = $row->minimumDepthInMeters;
+                $rowArr[] = $row->decimalLong;
+                $rowArr[] = $row->decimalLat;
+                foreach($configuredData['dataFields'] as $field => $fieldArr){
+                    $rowArr[] = (array_key_exists($eventId, $configuredDataArr) && array_key_exists($field, $configuredDataArr[$eventId])) ? $configuredDataArr[$eventId][$field] : '';
+                }
+                $returnArr[] = $rowArr;
+            }
+            $result->free();
         }
         return $returnArr;
     }
