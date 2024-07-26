66003db3ca917b5c8616477bbf8bd7b01fde21ae
Ben Brandt <benjamin.brandt@nau.edu>
2024-07-26 09:18:22 -0700

    WIP

--- taxa/dynamictreeviewer.php
+++ taxa/dynamictreeviewer.php
@@ -41,7 +41,7 @@ header('X-Frame-Options: SAMEORIGIN');
                                 <selector-input-element label="Type" :options="typeOptions" :value="selectedType" @update:value="setTreeType"></selector-input-element>
                             </div>
                             <div>
-                                <selector-input-element label="Layout Type" :options="layoutTypeOptions" :value="selectedLayoutType" @update:value="(value) => selectedLayoutType = value"></selector-input-element>
+                                <selector-input-element label="Layout Type" :options="layoutTypeOptions" :value="selectedLayoutType" @update:value="setLayoutType"></selector-input-element>
                             </div>
                             <div>
                                 <selector-input-element label="Link Layout" :options="linkLayoutOptions" :value="selectedLinkLayout" @update:value="setLinkLayout"></selector-input-element>
@@ -110,35 +110,108 @@ header('X-Frame-Options: SAMEORIGIN');
 
                     const diagonal = Vue.computed(() => {
                         if(selectedLinkLayout.value === 'bezier'){
-                            return d3.link(d3.curveBumpX).x(d => d.y).y(d => d.x);
+                            if(selectedLayoutType.value === 'horizontal'){
+                                return d3.link(d3.curveBumpX).x(d => d.y).y(d => d.x);
+                            }
+                            else if(selectedLayoutType.value === 'vertical'){
+                                return d3.link(d3.curveBumpY).x(d => d.x).y(d => d.y);
+                            }
+                            else{
+
+                            }
                         }
                         else{
-                            return d3.link(d3.curveStep).x(d => d.y).y(d => d.x);
+                            if(selectedLayoutType.value === 'horizontal'){
+                                return d3.link(d3.curveStep).x(d => d.y).y(d => d.x);
+                            }
+                            else if(selectedLayoutType.value === 'vertical'){
+                                return d3.link(d3.curveStepAfter).x(d => d.x).y(d => d.y);
+                            }
+                            else{
+
+                            }
                         }
                     });
                     const root = d3.hierarchy(treeData);
                     let tree = d3.tree().nodeSize([marginXValue.value, marginYValue.value]);
 
-                    const svg = d3.create("svg")
-                        .attr("width", containerWidth.value)
-                        .attr("height", containerHeight.value)
-                        .attr("viewBox", [(-1 * (containerWidth.value * 0.2)), (-1 * (containerHeight.value / 2)), containerWidth.value, containerHeight.value])
-                        .style("font", "10px sans-serif")
-                        .style("user-select", "none");
-                    const g = svg.append("g");
-                    const gLink = svg.append("g")
-                        .attr("fill", "none")
-                        .attr("stroke", "#555")
-                        .attr("stroke-opacity", 0.4)
-                        .attr("stroke-width", 1.5);
+                    const svg = d3.create('svg')
+                        .attr('width', containerWidth.value)
+                        .attr('height', containerHeight.value)
+                        .attr('viewBox', [(-1 * (containerWidth.value * 0.2)), (-1 * (containerHeight.value / 2)), containerWidth.value, containerHeight.value])
+                        .style('font', '10px sans-serif')
+                        .style('user-select', 'none');
+                    const g = svg.append('g');
+                    const gLink = svg.append('g')
+                        .attr('fill', 'none')
+                        .attr('stroke', '#555')
+                        .attr('stroke-opacity', 0.4)
+                        .attr('stroke-width', 1.5);
                     const gNode = svg.append("g")
-                        .attr("cursor", "pointer")
-                        .attr("pointer-events", "all");
+                        .attr('cursor', 'pointer')
+                        .attr('pointer-events', 'all');
+
+                    function processText() {
+                        d3.selectAll('text')
+                        .each(function() {
+                            const element = d3.select(this);
+                            const parentText = element.attr('text-type') === 'parent';
+                            element.attr('x', function() {
+                                if(selectedLayoutType.value === 'horizontal'){
+                                    return parentText ? (-1 * textMarginValue.value) : textMarginValue.value;
+                                }
+                                else if(selectedLayoutType.value === 'vertical'){
+                                    return parentText ? null : textMarginValue.value;
+                                }
+                                else{
+
+                                }
+                            });
+                            element.attr('y', function() {
+                                if(selectedLayoutType.value === 'horizontal'){
+                                    return null;
+                                }
+                                else if(selectedLayoutType.value === 'vertical'){
+                                    return parentText ? (-1 * textMarginValue.value) : null;
+                                }
+                                else{
+
+                                }
+                            });
+                            element.attr('text-anchor', function() {
+                                if(selectedLayoutType.value === 'horizontal'){
+                                    return parentText ? 'end' : 'start'
+                                }
+                                else if(selectedLayoutType.value === 'vertical'){
+                                    return 'middle'
+                                }
+                                else{
+
+                                }
+                            });
+                            element.attr('transform', function() {
+                                if(selectedLayoutType.value === 'horizontal'){
+                                    return null
+                                }
+                                else if(selectedLayoutType.value === 'vertical'){
+                                    return parentText ? null : 'rotate(90)'
+                                }
+                                else{
+
+                                }
+                            });
+                        });
+                    }
 
                     function setDimensions() {
                         containerHeight.value = treeDisplayRef.value.clientHeight;
                         containerWidth.value = treeDisplayRef.value.clientWidth;
                         setPng();
+                    }
+
+                    function setLayoutType(value) {
+                        selectedLayoutType.value = value;
+                        update(null, root);
                     }
 
                     function setLinkLayout(value) {
@@ -161,10 +234,10 @@ header('X-Frame-Options: SAMEORIGIN');
                     function setPng() {
                         const extent = [[0, 0], [containerWidth.value - 0, containerHeight.value - 0]];
                         svg.call(d3.zoom()
-                            .scaleExtent([1, 8])
+                            .scaleExtent([-100, 100])
                             .translateExtent(extent)
                             .extent(extent)
-                            .on("zoom", zoomed));
+                            .on('zoom', zoomed));
 
                         root.x0 = marginYValue.value / 2;
                         root.y0 = 0;
@@ -175,8 +248,8 @@ header('X-Frame-Options: SAMEORIGIN');
                                 d.children = null;
                             }
                         });
-                        update(null, root);
                         treeDisplayRef.value.append(svg.node());
+                        update(null, root);
                     }
 
                     function setRadius(value) {
@@ -186,8 +259,7 @@ header('X-Frame-Options: SAMEORIGIN');
 
                     function setTextMargin(value) {
                         textMarginValue.value = value;
-                        d3.selectAll('.parent-text-label').attr('x', (-1 * textMarginValue.value));
-                        d3.selectAll('.child-text-label').attr('x', textMarginValue.value);
+                        processText();
                     }
 
                     function setTreeType(value) {
@@ -223,63 +295,91 @@ header('X-Frame-Options: SAMEORIGIN');
 
                         const transition = svg.transition()
                             .duration(duration)
-                            .attr("width", containerWidth.value)
-                            .attr("height", containerHeight.value)
-                            .attr("viewBox", [(-1 * (containerWidth.value * 0.2)), (-1 * (containerHeight.value / 2)), containerWidth.value, height])
-                            .tween("resize", window.ResizeObserver ? null : () => () => svg.dispatch("toggle"));
+                            .attr('width', containerWidth.value)
+                            .attr('height', containerHeight.value)
+                            .attr('viewBox', [(-1 * (containerWidth.value * 0.2)), (-1 * (containerHeight.value / 2)), containerWidth.value, height])
+                            .tween('resize', window.ResizeObserver ? null : () => () => svg.dispatch('toggle'));
 
                         const node = gNode.selectAll("g")
                             .data(nodes, d => d.id);
 
-                        const nodeEnter = node.enter().append("g")
-                            .attr("transform", d => `translate(${source.y0},${source.x0})`)
-                            .attr("fill-opacity", 0)
-                            .attr("stroke-opacity", 0)
-                            .on("click", (event, d) => {
+                        const nodeEnter = node.enter().append('g')
+                            .attr('transform', d => {
+                                if(selectedLayoutType.value === 'horizontal'){
+                                    return `translate(${source.y0},${source.x0})`
+                                }
+                                else if(selectedLayoutType.value === 'vertical'){
+                                    return `translate(${source.x0},${source.y0})`
+                                }
+                                else{
+
+                                }
+                            })
+                            .attr('fill-opacity', 0)
+                            .attr('stroke-opacity', 0)
+                            .on('click', (event, d) => {
                                 d.children = d.children ? null : d._children;
                                 update(event, d);
                             });
 
-                        nodeEnter.append("circle")
-                            .attr("r", radiusValue.value)
-                            .attr("fill", d => d._children ? "#555" : "#999")
-                            .attr("stroke-width", 10);
+                        nodeEnter.append('circle')
+                            .attr('r', radiusValue.value)
+                            .attr('fill', d => d._children ? "#555" : "#999")
+                            .attr('stroke-width', 10);
 
-                        nodeEnter.append("text")
-                            .attr("dy", "0.25em")
-                            .attr("x", d => d._children ? (-1 * textMarginValue.value) : textMarginValue.value)
-                            .attr("class", d => d._children ? 'parent-text-label' : 'child-text-label')
-                            .attr("text-anchor", d => d._children ? "end" : "start")
+                        nodeEnter.append('text')
+                            .attr('dy', '0.25em')
+                            .attr('text-type', d => d._children ? 'parent' : 'child')
                             .text(d => d.data.name)
-                            .attr("stroke-linejoin", "round")
-                            .attr("stroke-width", 3)
-                            .attr("stroke", "white")
-                            .attr("paint-order", "stroke");
+                            .attr('stroke-linejoin', 'round')
+                            .attr('stroke-width', 3)
+                            .attr('stroke', 'white')
+                            .attr('paint-order', 'stroke');
 
                         node.merge(nodeEnter).transition(transition)
-                            .attr("transform", d => `translate(${d.y},${d.x})`)
-                            .attr("fill-opacity", 1)
-                            .attr("stroke-opacity", 1);
+                            .attr('transform', d => {
+                                if(selectedLayoutType.value === 'horizontal'){
+                                    return `translate(${d.y},${d.x})`
+                                }
+                                else if(selectedLayoutType.value === 'vertical'){
+                                    return `translate(${d.x},${d.y})`
+                                }
+                                else{
+
+                                }
+                            })
+                            .attr('fill-opacity', 1)
+                            .attr('stroke-opacity', 1);
 
                         node.exit().transition(transition).remove()
-                            .attr("transform", d => `translate(${source.y},${source.x})`)
-                            .attr("fill-opacity", 0)
-                            .attr("stroke-opacity", 0);
+                            .attr('transform', d => {
+                                if(selectedLayoutType.value === 'horizontal'){
+                                    return `translate(${source.y},${source.x})`
+                                }
+                                else if(selectedLayoutType.value === 'vertical'){
+                                    return `translate(${source.x},${source.y})`
+                                }
+                                else{
 
-                        const link = gLink.selectAll("path")
+                                }
+                            })
+                            .attr('fill-opacity', 0)
+                            .attr('stroke-opacity', 0);
+
+                        const link = gLink.selectAll('path')
                             .data(links, d => d.target.id);
 
-                        const linkEnter = link.enter().append("path")
+                        const linkEnter = link.enter().append('path')
                             .attr("d", d => {
                                 const o = {x: source.x0, y: source.y0};
                                 return diagonal.value({source: o, target: o});
                             });
 
                         link.merge(linkEnter).transition(transition)
-                            .attr("d", diagonal.value);
+                            .attr('d', diagonal.value);
 
                         link.exit().transition(transition).remove()
-                            .attr("d", d => {
+                            .attr('d', d => {
                                 const o = {x: source.x, y: source.y};
                                 return diagonal.value({source: o, target: o});
                             });
@@ -288,10 +388,12 @@ header('X-Frame-Options: SAMEORIGIN');
                             d.x0 = d.x;
                             d.y0 = d.y;
                         });
+
+                        processText();
                     }
 
                     function zoomed({transform}) {
-                        svg.attr("transform", transform);
+                        svg.attr('transform', transform);
                     }
 
                     Vue.onMounted(() => {
@@ -312,6 +414,7 @@ header('X-Frame-Options: SAMEORIGIN');
                         treeData,
                         treeDisplayRef,
                         typeOptions,
+                        setLayoutType,
                         setLinkLayout,
                         setMarginX,
                         setMarginY,
