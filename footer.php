357cccb709c520bf746d4f5dab30e5e5215cea32
Ben Brandt <benjamin.brandt@nau.edu>
2024-08-01 12:25:02 -0700

    Several updates

--- footer.php
+++ footer.php
@@ -5,27 +5,7 @@ include_once(__DIR__ . '/services/SanitizerService.php');
     <div class="q-pa-md full-width row justify-between">
         <div class="col-grow column">
             <div class="full-width q-pt-xs q-pb-sm q-pl-sm q-pr-lg">
-                <q-card flat bordered class="black-border bg-grey-3">
-                    <q-card-section class="q-pa-sm column">
-                        <div class="q-mb-xs row justify-between">
-                            <div>
-                                <q-btn-toggle v-model="selectedTaxonType" :options="taxonTypeOptions" class="black-border" size="sm" rounded unelevated toggle-color="primary" color="white" text-color="primary" @update:model-value="processTaxonTypeChange"></q-btn-toggle>
-                            </div>
-                            <div class="row justify-end">
-                                <div>
-                                    <a class="text-body1 text-bold" :href="(clientRoot + '/taxa/dynamictaxalist.php')">
-                                        Advanced Search
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-grow">
-                                <taxa-search-auto-complete :label="autoCompleteLabel" :taxon-type="taxonType"></taxa-search-auto-complete>
-                            </div>
-                        </div>
-                    </q-card-section>
-                </q-card>
+                <taxa-quick-search></taxa-quick-search>
             </div>
             <div class="row justify-center q-col-gutter-sm">
                 <div class="col-6">
@@ -135,230 +115,16 @@ include_once(__DIR__ . '/services/SanitizerService.php');
     </q-dialog>
 </div>
 <script>
-    const taxaSearchAutoComplete = {
-        props: {
-            hideAuthor: {
-                type: Boolean,
-                default: true
-            },
-            hideProtected: {
-                type: Boolean,
-                default: false
-            },
-            label: {
-                type: String,
-                default: 'Scientific Name'
-            },
-            limitToThesaurus: {
-                type: Boolean,
-                default: false
-            },
-            optionLimit: {
-                type: Number,
-                default: 10
-            },
-            rankHigh: {
-                type: Number,
-                default: null
-            },
-            rankLimit: {
-                type: Number,
-                default: null
-            },
-            rankLow: {
-                type: Number,
-                default: null
-            },
-            taxonType: {
-                type: Number,
-                default: null
-            }
-        },
-        template: `
-            <q-select v-model="selectedTaxon.label" use-input hide-selected fill-input outlined dense options-dense hide-dropdown-icon popup-content-class="z-max" input-debounce="0" bg-color="white" @new-value="createValue" :options="autocompleteOptions" @filter="getOptions" @blur="blurAction" @update:model-value="processChange" :label="label">
-                <template v-slot:append>
-                    <q-icon name="search" class="cursor-pointer" @click="processSearch();">
-                        <q-tooltip anchor="top middle" self="bottom middle" class="text-body2" :delay="1000" :offset="[10, 10]">
-                            Clear value
-                        </q-tooltip>
-                    </q-icon>
-                </template>
-            </q-select>
-        `,
-        setup(props) {
-            const { showNotification } = useCore();
-            const store = useBaseStore();
-
-            const autocompleteOptions = Vue.ref([]);
-            const clientRoot = store.getClientRoot;
-            const propsRefs = Vue.toRefs(props);
-            const selectedTaxon = Vue.ref({});
-
-            Vue.watch(propsRefs.taxonType, () => {
-                selectedTaxon.value = Object.assign({}, {});
-            });
-
-            function blurAction(val) {
-                if(val && selectedTaxon.value && val.target.value !== selectedTaxon.value.label){
-                    const optionObj = autocompleteOptions.value.find(option => option['sciname'] === val.target.value);
-                    if(optionObj){
-                        processChange(optionObj);
-                    }
-                    else if(!props.limitToThesaurus){
-                        processChange({
-                            label: val.target.value,
-                            sciname: val.target.value,
-                            tid: null,
-                            family: null,
-                            author: null
-                        });
-                    }
-                    else{
-                        showNotification('negative', 'That name was not found in the Taxonomic Thesaurus.');
-                    }
-                }
-            }
-
-            function createValue(val, done) {
-                if(val.length > 0) {
-                    const optionObj = autocompleteOptions.value.find(option => option['sciname'] === val);
-                    if(optionObj){
-                        done(optionObj, 'add');
-                    }
-                    else if(!props.limitToThesaurus){
-                        done({
-                            label: val,
-                            sciname: val,
-                            tid: null,
-                            family: null,
-                            author: null
-                        }, 'add');
-                    }
-                    else{
-                        showNotification('negative', 'That name was not found in the Taxonomic Thesaurus.');
-                    }
-                }
-            }
-
-            function getOptions(val, update) {
-                update(() => {
-                    if(val.length > 2) {
-                        let action = 'getAutocompleteSciNameList';
-                        let rankLimit, rankLow, rankHigh;
-                        let dataSource = taxaApiUrl;
-                        if(props.taxonType){
-                            if(Number(props.taxonType) === 1){
-                                rankLow = 140;
-                            }
-                            else if(Number(props.taxonType) === 2){
-                                rankLimit = 140;
-                            }
-                            else if(Number(props.taxonType) === 3){
-                                rankLow = 180;
-                            }
-                            else if(Number(props.taxonType) === 4){
-                                rankLow = 10;
-                                rankHigh = 130;
-                            }
-                            else if(Number(props.taxonType) === 6){
-                                action = 'getAutocompleteVernacularList';
-                                dataSource = taxonVernacularApiUrl;
-                            }
-                        }
-                        else{
-                            rankLimit = props.rankLimit;
-                            rankLow = props.rankLow;
-                            rankHigh = props.rankHigh;
-                        }
-                        const formData = new FormData();
-                        formData.append('action', action);
-                        formData.append('term', val);
-                        formData.append('hideauth', props.hideAuthor);
-                        formData.append('hideprotected', props.hideProtected);
-                        formData.append('rlimit', rankLimit);
-                        formData.append('rlow', rankLow);
-                        formData.append('rhigh', rankHigh);
-                        formData.append('limit', props.optionLimit);
-                        fetch(dataSource, {
-                            method: 'POST',
-                            body: formData
-                        })
-                        .then((response) => response.json())
-                        .then((result) => {
-                            autocompleteOptions.value = result;
-                        });
-                    }
-                    else{
-                        autocompleteOptions.value = [];
-                    }
-                });
-            }
-
-            function processChange(taxonObj) {
-                selectedTaxon.value = Object.assign({}, taxonObj);
-            }
-
-            function processSearch() {
-                if(Number(props.taxonType) === 6){
-                    const formData = new FormData();
-                    formData.append('vernacular', selectedTaxon.value['label']);
-                    formData.append('action', 'getHighestRankingTidByVernacular');
-                    fetch(taxonVernacularApiUrl, {
-                        method: 'POST',
-                        body: formData
-                    })
-                    .then((response) => {
-                        if(response.status === 200){
-                            response.text().then((res) => {
-                                if(Number(res) > 0){
-                                    window.location.href = (clientRoot + '/taxa/index.php?taxon=' + res);
-                                }
-                                else{
-                                    showNotification('negative', 'That common name was not found in the database');
-                                }
-                            });
-                        }
-                    });
-                }
-                else{
-                    if(selectedTaxon.value.hasOwnProperty('tid') && Number(selectedTaxon.value['tid']) > 0){
-                        window.location.href = (clientRoot + '/taxa/index.php?taxon=' + selectedTaxon.value['tid']);
-                    }
-                    else{
-                        showNotification('negative', 'That scientific name was not found in the database');
-                    }
-                }
-            }
-
-            return {
-                autocompleteOptions,
-                selectedTaxon,
-                blurAction,
-                createValue,
-                getOptions,
-                processChange,
-                processSearch
-            }
-        }
-    };
-
     const footerElement = Vue.createApp({
         components: {
-            'taxa-search-auto-complete': taxaSearchAutoComplete
+            'taxa-quick-search': taxaQuickSearch
         },
         setup() {
             const store = useBaseStore();
 
-            const autoCompleteLabel = Vue.ref('Common Name');
             const clientRoot = store.getClientRoot;
             const donateConfirm = Vue.ref(false);
-            const selectedTaxonType = Vue.ref('common');
             const storeRefs = Pinia.storeToRefs(store);
-            const taxonTypeOptions = [
-                {label: 'Common Name', value: 'common'},
-                {label: 'Scientific Name', value: 'scientific'}
-            ];
-            const taxonType = Vue.ref(6);
             const userDisplayName = storeRefs.getUserDisplayName;
 
             function logout() {
@@ -373,28 +139,12 @@ include_once(__DIR__ . '/services/SanitizerService.php');
                 window.open('https://support.si.edu/site/Donation2;jsessionid=00000000.app30030a?idb=172924536&df_id=19745&mfc_pref=T&19745.donation=form1&NONCE_TOKEN=B8237A09ED48545AB4117EA7BD9F20EF&s_subsrc=top-btn&s_src=main-web&autologin=true&19745_donation=form1', '_blank');
             }
 
-            function processTaxonTypeChange(value) {
-                if(value === 'common'){
-                    autoCompleteLabel.value = 'Common Name';
-                    taxonType.value = 6;
-                }
-                else{
-                    autoCompleteLabel.value = 'Scientific Name';
-                    taxonType.value = null;
-                }
-            }
-
             return {
-                autoCompleteLabel,
                 clientRoot,
                 donateConfirm,
-                selectedTaxonType,
-                taxonTypeOptions,
                 userDisplayName,
-                taxonType,
                 logout,
-                openDonatePage,
-                processTaxonTypeChange
+                openDonatePage
             };
         }
     });
--- index.php
+++ index.php
@@ -23,18 +23,6 @@ include_once(__DIR__ . '/config/header-includes.php');
     <script src="js/external/jquery.js" type="text/javascript"></script>
     <script src="js/external/jquery-ui.js" type="text/javascript"></script>
     <script type="text/javascript">
-        $(document).ready(function() {
-            const quicksearchDiv = document.getElementById('quicksearchinputcontainer');
-            const linkDiv = document.createElement('div');
-            linkDiv.setAttribute("class","as");
-            const linkElement = document.createElement('a');
-            linkElement.setAttribute("href","<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/dynamictaxalist.php");
-            linkElement.innerHTML = "Advanced Search";
-            linkDiv.appendChild(linkElement);
-            quicksearchDiv.appendChild(linkDiv);
-         });
-    </script>
-    <script type="text/javascript">
         var imgArray = [
             "url(content/imglib/static/09ThomanR1.jpg)",
             "url(content/imglib/static/20Donahue1.jpg)",
@@ -114,28 +102,12 @@ include_once(__DIR__ . '/config/header-includes.php');
             include(__DIR__ . '/header-navigation.php');
             ?>
         </div>
-        <div id="quicksearch-container">
-            <q-card flat bordered class="black-border bg-grey-3">
-                <q-card-section class="q-pa-sm column">
-                    <div class="q-mb-xs row justify-between">
-                        <div>
-                            <q-btn-toggle v-model="selectedTaxonType" :options="taxonTypeOptions" class="black-border" size="sm" rounded unelevated toggle-color="primary" color="white" text-color="primary" @update:model-value="processTaxonTypeChange"></q-btn-toggle>
-                        </div>
-                        <div class="row justify-end">
-                            <div>
-                                <a class="text-body1 text-bold" :href="(clientRoot + '/taxa/dynamictaxalist.php')">
-                                    Advanced Search
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-grow">
-                            <taxa-search-auto-complete :label="autoCompleteLabel" :taxon-type="taxonType"></taxa-search-auto-complete>
-                        </div>
-                    </div>
-                </q-card-section>
-            </q-card>
+        <div id="quicksearch-container" class="quicksearch-container">
+            <div class="full-width row justify-center">
+                <div class="col-6">
+                    <taxa-quick-search></taxa-quick-search>
+                </div>
+            </div>
         </div>
         <div class="heading-container">
             <div class="heading-inner">
@@ -170,228 +142,14 @@ include_once(__DIR__ . '/config/header-includes.php');
     include(__DIR__ . '/footer.php');
     ?>
     <script>
-        const taxaSearchAutoComplete = {
-            props: {
-                hideAuthor: {
-                    type: Boolean,
-                    default: true
-                },
-                hideProtected: {
-                    type: Boolean,
-                    default: false
-                },
-                label: {
-                    type: String,
-                    default: 'Scientific Name'
-                },
-                limitToThesaurus: {
-                    type: Boolean,
-                    default: false
-                },
-                optionLimit: {
-                    type: Number,
-                    default: 10
-                },
-                rankHigh: {
-                    type: Number,
-                    default: null
-                },
-                rankLimit: {
-                    type: Number,
-                    default: null
-                },
-                rankLow: {
-                    type: Number,
-                    default: null
-                },
-                taxonType: {
-                    type: Number,
-                    default: null
-                }
-            },
-            template: `
-            <q-select v-model="selectedTaxon.label" use-input hide-selected fill-input outlined dense options-dense hide-dropdown-icon popup-content-class="z-max" input-debounce="0" bg-color="white" @new-value="createValue" :options="autocompleteOptions" @filter="getOptions" @blur="blurAction" @update:model-value="processChange" :label="label">
-                <template v-slot:append>
-                    <q-icon name="search" class="cursor-pointer" @click="processSearch();">
-                        <q-tooltip anchor="top middle" self="bottom middle" class="text-body2" :delay="1000" :offset="[10, 10]">
-                            Clear value
-                        </q-tooltip>
-                    </q-icon>
-                </template>
-            </q-select>
-        `,
-            setup(props) {
-                const { showNotification } = useCore();
-                const store = useBaseStore();
-
-                const autocompleteOptions = Vue.ref([]);
-                const clientRoot = store.getClientRoot;
-                const propsRefs = Vue.toRefs(props);
-                const selectedTaxon = Vue.ref({});
-
-                Vue.watch(propsRefs.taxonType, () => {
-                    selectedTaxon.value = Object.assign({}, {});
-                });
-
-                function blurAction(val) {
-                    if(val && selectedTaxon.value && val.target.value !== selectedTaxon.value.label){
-                        const optionObj = autocompleteOptions.value.find(option => option['sciname'] === val.target.value);
-                        if(optionObj){
-                            processChange(optionObj);
-                        }
-                        else if(!props.limitToThesaurus){
-                            processChange({
-                                label: val.target.value,
-                                sciname: val.target.value,
-                                tid: null,
-                                family: null,
-                                author: null
-                            });
-                        }
-                        else{
-                            showNotification('negative', 'That name was not found in the Taxonomic Thesaurus.');
-                        }
-                    }
-                }
-
-                function createValue(val, done) {
-                    if(val.length > 0) {
-                        const optionObj = autocompleteOptions.value.find(option => option['sciname'] === val);
-                        if(optionObj){
-                            done(optionObj, 'add');
-                        }
-                        else if(!props.limitToThesaurus){
-                            done({
-                                label: val,
-                                sciname: val,
-                                tid: null,
-                                family: null,
-                                author: null
-                            }, 'add');
-                        }
-                        else{
-                            showNotification('negative', 'That name was not found in the Taxonomic Thesaurus.');
-                        }
-                    }
-                }
-
-                function getOptions(val, update) {
-                    update(() => {
-                        if(val.length > 2) {
-                            let action = 'getAutocompleteSciNameList';
-                            let rankLimit, rankLow, rankHigh;
-                            let dataSource = taxaApiUrl;
-                            if(props.taxonType){
-                                if(Number(props.taxonType) === 1){
-                                    rankLow = 140;
-                                }
-                                else if(Number(props.taxonType) === 2){
-                                    rankLimit = 140;
-                                }
-                                else if(Number(props.taxonType) === 3){
-                                    rankLow = 180;
-                                }
-                                else if(Number(props.taxonType) === 4){
-                                    rankLow = 10;
-                                    rankHigh = 130;
-                                }
-                                else if(Number(props.taxonType) === 6){
-                                    action = 'getAutocompleteVernacularList';
-                                    dataSource = taxonVernacularApiUrl;
-                                }
-                            }
-                            else{
-                                rankLimit = props.rankLimit;
-                                rankLow = props.rankLow;
-                                rankHigh = props.rankHigh;
-                            }
-                            const formData = new FormData();
-                            formData.append('action', action);
-                            formData.append('term', val);
-                            formData.append('hideauth', props.hideAuthor);
-                            formData.append('hideprotected', props.hideProtected);
-                            formData.append('rlimit', rankLimit);
-                            formData.append('rlow', rankLow);
-                            formData.append('rhigh', rankHigh);
-                            formData.append('limit', props.optionLimit);
-                            fetch(dataSource, {
-                                method: 'POST',
-                                body: formData
-                            })
-                                .then((response) => response.json())
-                                .then((result) => {
-                                    autocompleteOptions.value = result;
-                                });
-                        }
-                        else{
-                            autocompleteOptions.value = [];
-                        }
-                    });
-                }
-
-                function processChange(taxonObj) {
-                    selectedTaxon.value = Object.assign({}, taxonObj);
-                }
-
-                function processSearch() {
-                    if(Number(props.taxonType) === 6){
-                        const formData = new FormData();
-                        formData.append('vernacular', selectedTaxon.value['label']);
-                        formData.append('action', 'getHighestRankingTidByVernacular');
-                        fetch(taxonVernacularApiUrl, {
-                            method: 'POST',
-                            body: formData
-                        })
-                            .then((response) => {
-                                if(response.status === 200){
-                                    response.text().then((res) => {
-                                        if(Number(res) > 0){
-                                            window.location.href = (clientRoot + '/taxa/index.php?taxon=' + res);
-                                        }
-                                        else{
-                                            showNotification('negative', 'That common name was not found in the database');
-                                        }
-                                    });
-                                }
-                            });
-                    }
-                    else{
-                        if(selectedTaxon.value.hasOwnProperty('tid') && Number(selectedTaxon.value['tid']) > 0){
-                            window.location.href = (clientRoot + '/taxa/index.php?taxon=' + selectedTaxon.value['tid']);
-                        }
-                        else{
-                            showNotification('negative', 'That scientific name was not found in the database');
-                        }
-                    }
-                }
-
-                return {
-                    autocompleteOptions,
-                    selectedTaxon,
-                    blurAction,
-                    createValue,
-                    getOptions,
-                    processChange,
-                    processSearch
-                }
-            }
-        };
-
-        const quicksearchElement = Vue.createApp({
+        const quickSearchElement = Vue.createApp({
             components: {
-                'taxa-search-auto-complete': taxaSearchAutoComplete
-            },
-            setup() {
-
-
-                return {
-
-                };
+                'taxa-quick-search': taxaQuickSearch
             }
         });
-        quicksearchElement.use(Quasar, { config: {} });
-        quicksearchElement.use(Pinia.createPinia());
-        quicksearchElement.mount('#quicksearch-container');
+        quickSearchElement.use(Quasar, { config: {} });
+        quickSearchElement.use(Pinia.createPinia());
+        quickSearchElement.mount('#quicksearch-container');
     </script>
 </body>
 </html>
