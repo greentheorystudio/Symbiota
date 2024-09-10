5f98ebbb979d879b3c4c86b5b7fe530d466060f6
Ben Brandt <benjamin.brandt@nau.edu>
2024-09-10 09:23:19 -0700

    WIP

--- models/Configurations.php
+++ models/Configurations.php
@@ -430,7 +430,7 @@ class Configurations{
             $GLOBALS['DEFAULT_TITLE'] = '';
         }
         $GLOBALS['CSS_VERSION'] = '20240801';
-        $GLOBALS['JS_VERSION'] = '2024050311111111111122222222222222333334444655';
+        $GLOBALS['JS_VERSION'] = '20240504';
         $GLOBALS['PARAMS_ARR'] = array();
         $GLOBALS['USER_RIGHTS'] = array();
         $this->validateGlobalArr();
