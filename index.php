0f72eede1e675df96f967554e3b7d9f47eedc0c5
Ben Brandt <benjamin.brandt@nau.edu>
2024-11-14 19:31:58 -0700

    Styling update

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
 include_once(__DIR__ . '/config/symbbase.php');
-header('Content-Type: text/html; charset=' .$GLOBALS['CHARSET']);
+header('Content-Type: text/html; charset=UTF-8' );
 ?>
+<!DOCTYPE html>
 <html lang="<?php echo $GLOBALS['DEFAULT_LANG']; ?>">
+<?php
+include_once(__DIR__ . '/config/header-includes.php');
+?>
 <head>
     <title><?php echo $GLOBALS['DEFAULT_TITLE']; ?> Home</title>
-    <link href="css/base.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" type="text/css" rel="stylesheet" />
-    <link href="css/main.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" type="text/css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/base.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" rel="stylesheet" type="text/css" />
+    <link href="css/main.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" rel="stylesheet" type="text/css" />
     <meta name='keywords' content='' />
-    <?php include_once('config/googleanalytics.php'); ?>
 </head>
 <body>
 <?php
@@ -53,6 +57,7 @@ include(__DIR__ . '/header.php');
 </div>
 
 <?php
+include_once(__DIR__ . '/config/footer-includes.php');
 include(__DIR__ . '/footer.php');
 ?>
 </body>
