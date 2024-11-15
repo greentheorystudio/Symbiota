7e03cd6acef5428ffe6c2adb5ddc610d2269cc22
Ben Brandt <benjamin.brandt@nau.edu>
2024-11-15 15:07:52 -0700

    WIP

--- components/search/copyURLButton.js
+++ components/search/copyURLButton.js
@@ -6,7 +6,7 @@ const copyURLButton = {
         }
     },
     template: `
-        <div class="self-center">
+        <div v-if="secureOrigin" class="self-center">
             <q-btn color="grey-4" text-color="black" class="black-border" size="md" @click="copySearchUrlToClipboard(pageNumber);" icon="fas fa-link" dense>
                 <q-tooltip anchor="top middle" self="bottom middle" class="text-body2" :delay="1000" :offset="[10, 10]">
                     Copy URL to Clipboard
@@ -19,27 +19,26 @@ const copyURLButton = {
         const searchStore = useSearchStore();
 
         const searchTerms = Vue.computed(() => searchStore.getSearchTerms);
+        const secureOrigin = Vue.computed(() => {
+            return (window.location.host === 'localhost' || window.location.protocol === 'https:');
+        });
 
         function copySearchUrlToClipboard(index) {
-            if(navigator && navigator.hasOwnProperty('clipboard')){
-                const currentSearchTerms = Object.assign({}, searchTerms.value);
-                currentSearchTerms.recordPage = index;
-                const searchTermsJson = JSON.stringify(currentSearchTerms);
-                let copyUrl = window.location.href + '?starr=' + searchTermsJson.replaceAll("'", '%squot;');
-                navigator.clipboard.writeText(copyUrl)
-                .then(() => {
-                    showNotification('positive','URL copied successfully');
-                })
-                .catch(() => {
-                    showNotification('negative', 'An error occurred while copying the URL to the clipboard');
-                });
-            }
-            else{
+            const currentSearchTerms = Object.assign({}, searchTerms.value);
+            currentSearchTerms.recordPage = index;
+            const searchTermsJson = JSON.stringify(currentSearchTerms);
+            let copyUrl = window.location.href + '?starr=' + searchTermsJson.replaceAll("'", '%squot;');
+            navigator.clipboard.writeText(copyUrl)
+            .then(() => {
+                showNotification('positive','URL copied successfully');
+            })
+            .catch(() => {
                 showNotification('negative', 'An error occurred while copying the URL to the clipboard');
-            }
+            });
         }
 
         return {
+            secureOrigin,
             copySearchUrlToClipboard
         }
     }
