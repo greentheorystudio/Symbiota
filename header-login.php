00b30fc985b31f6bc475e5967e0a1f394fb89c79
Ben Brandt <benjamin.brandt@nau.edu>
2025-04-10 18:03:06 -0700

    Several updates

--- header-login.php
+++ header-login.php
@@ -26,9 +26,9 @@ include_once(__DIR__ . '/services/SanitizerService.php');
                 logout() {
                     const url = profileApiUrl + '?action=logout';
                     fetch(url)
-                        .then(() => {
-                            window.location.href = CLIENT_ROOT + '/index.php';
-                        })
+                    .then(() => {
+                        window.location.href = CLIENT_ROOT + '/index.php';
+                    })
                 }
             }
         });
