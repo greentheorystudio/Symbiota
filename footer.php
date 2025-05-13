60ebc60a7469c6af5f3de612023ee528b3b0f20d
Ben Brandt <benjamin.brandt@nau.edu>
2025-05-12 20:42:13 -0700

    Update

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
-<div class="footer">
-    <div style="width: 100%; display: flex; flex-wrap: nowrap; align-items: center; justify-content: center;">
-        <div style="margin: 2rem;">
-            <a href="https://www.sbbg.org" target="_blank">
-                <img src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/content/imglib/layout/SBBG_Lockup_Charcoal_CMYK.jpg" style="width: 12rem;" alt="SBBG logo">
-            </a>
-        </div>
-        <div style="margin: 2rem;">
-            <a href="https://www.nature.org" target="_blank">
-                <img src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/content/imglib/layout/TNCLogoPrimary_RGB.png" style="width: 12rem;" alt="Nature conservancy logo">
-            </a>
+    <div id="footer">
+        <div style="width: 100%; display: flex; flex-wrap: nowrap; align-items: center; justify-content: center;">
+            <div style="margin: 2rem;">
+                <a href="https://www.sbbg.org" target="_blank">
+                    <img src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/content/imglib/layout/SBBG_Lockup_Charcoal_CMYK.jpg" style="width: 12rem;" alt="SBBG logo">
+                </a>
+            </div>
+            <div style="margin: 2rem;">
+                <a href="https://www.nature.org" target="_blank">
+                    <img src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/content/imglib/layout/TNCLogoPrimary_RGB.png" style="width: 12rem;" alt="Nature conservancy logo">
+                </a>
+            </div>
         </div>
     </div>
-</div>
 </div>
