a03b145c71eae54d68b8dbf21d696450ecae022f
Ben Brandt <benjamin.brandt@nau.edu>
2025-08-11 15:51:11 -0700

    Update

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
-include_once(__DIR__ . '/classes/Sanitizer.php');
+include_once(__DIR__ . '/services/SanitizerService.php');
 ?>
-<div id="mainContainer">
+<div id="appContainer">
     <div class="uw-global-bar uw-global-bar-inverse" role="navigation">
         <a class="uw-global-name-link" href="https://www.wisc.edu" aria-label="University of Wisconsin Madison home page">U<span>niversity <span class="uw-of">of</span> </span>W<span>isconsin</span>–Madison</a>
     </div>
@@ -37,7 +37,7 @@ include_once(__DIR__ . '/classes/Sanitizer.php');
     <div id="topNavigation">
         <q-toolbar class="q-pa-md horizontalDropDown">
             <template v-if="windowWidth < 1440">
-                <q-btn class="horizontalDropDownIconButton q-ml-md" flat round dense icon="menu">
+                <q-btn class="horizontalDropDownIconButton q-ml-md" flat round dense icon="menu" aria-label="Menu">
                     <q-menu>
                         <q-list dense>
                             <template v-for="item in navBarData">
@@ -90,30 +90,35 @@ include_once(__DIR__ . '/classes/Sanitizer.php');
             <q-space></q-space>
             <template v-if="userDisplayName">
                 <q-breadcrumbs-el class="header-username-text">Welcome {{ userDisplayName }}!</q-breadcrumbs-el>
-                <q-btn class="horizontalDropDownButton text-capitalize" href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/profile/viewprofile.php" label="My Profile" stretch flat no-wrap></q-btn>
+                <q-btn class="horizontalDropDownButton text-capitalize" :href="clientRoot + '/profile/viewprofile.php'" label="My Profile" stretch flat no-wrap></q-btn>
                 <q-btn class="horizontalDropDownButton text-capitalize" @click="logout();" label="Logout" stretch flat no-wrap></q-btn>
             </template>
             <template v-else>
-                <q-btn class="horizontalDropDownButton text-capitalize" href="<?php echo $GLOBALS['CLIENT_ROOT']. '/profile/index.php?refurl=' .Sanitizer::getCleanedRequestPath(true); ?>" label="Log In" stretch flat no-wrap></q-btn>
-                <q-btn class="horizontalDropDownButton text-capitalize" href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/profile/newprofile.php" label="New Account" stretch flat no-wrap></q-btn>
+                <q-btn class="horizontalDropDownButton text-capitalize" :href="clientRoot + '/profile/index.php?refurl=' + requestPath" label="Log In" stretch flat no-wrap></q-btn>
+                <q-btn class="horizontalDropDownButton text-capitalize" :href="clientRoot + '/profile/newprofile.php'" label="New Account" stretch flat no-wrap></q-btn>
             </template>
-            <q-btn class="horizontalDropDownButton text-capitalize" href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/sitemap.php" label="Sitemap" stretch flat no-wrap></q-btn>
+            <q-btn class="horizontalDropDownButton text-capitalize" :href="clientRoot + '/sitemap.php'" label="Sitemap" stretch flat no-wrap></q-btn>
         </q-toolbar>
     </div>
-    <script>
+    <script type="text/javascript">
+        const REQUEST_PATH = "<?php echo SanitizerService::getCleanedRequestPath(true); ?>";
         document.addEventListener("DOMContentLoaded", () => {
             const dropDownNavBar = Vue.createApp({
                 setup() {
+                    const store = useBaseStore();
+                    const storeRefs = Pinia.storeToRefs(store);
+                    const clientRoot = store.getClientRoot;
                     const navBarData = Vue.ref([
-                        {url: CLIENT_ROOT + '/index.php', label: 'Home'},
-                        {url: CLIENT_ROOT + '/collections/index.php', label: 'Search Collections'},
-                        {url: CLIENT_ROOT + '/spatial/index.php', label: 'Spatial Module', newTab: true},
-                        {url: CLIENT_ROOT + '/imagelib/search.php', label: 'Image Search'},
-                        {url: CLIENT_ROOT + '/imagelib/index.php', label: 'Browse Images'}
+                        {url: clientRoot + '/index.php', label: 'Home'},
+                        {url: clientRoot + '/collections/list.php', label: 'Search Collections'},
+                        {url: clientRoot + '/spatial/index.php', label: 'Spatial Module', newTab: true},
+                        {url: clientRoot + '/imagelib/search.php', label: 'Image Search'},
+                        {url: clientRoot + '/imagelib/index.php', label: 'Browse Images'}
                     ]);
                     let navBarTimeout = null;
                     const navBarToggle = Vue.ref({});
-                    const userDisplayName = USER_DISPLAY_NAME;
+                    const requestPath = REQUEST_PATH;
+                    const userDisplayName = storeRefs.getUserDisplayName;
                     const windowWidth = Vue.ref(0);
 
                     function  handleResize() {
@@ -124,8 +129,24 @@ include_once(__DIR__ . '/classes/Sanitizer.php');
                         const url = profileApiUrl + '?action=logout';
                         fetch(url)
                             .then(() => {
-                                window.location.href = CLIENT_ROOT + '/index.php';
+                                window.location.href = clientRoot + '/index.php';
                             })
+                    }
+
+                    function navbarToggleOff(id) {
+                        this.navBarTimeout = setTimeout(() => {
+                            this.navBarToggle[Number(id)] = false;
+                        }, 400);
+                    }
+
+                    function navbarToggleOn(id) {
+                        clearTimeout(this.navBarTimeout);
+                        for(let i in this.navBarToggle){
+                            if(this.navBarToggle.hasOwnProperty(i) && Number(i) !== Number(id)){
+                                this.navBarToggle[Number(i)] = false;
+                            }
+                        }
+                        this.navBarToggle[Number(id)] = true;
                     }
 
                     function setNavBarData() {
@@ -144,34 +165,23 @@ include_once(__DIR__ . '/classes/Sanitizer.php');
                     });
 
                     return {
+                        clientRoot,
                         navBarData,
                         navBarToggle,
                         navBarTimeout,
+                        requestPath,
                         userDisplayName,
                         windowWidth,
+                        navbarToggleOff,
+                        navbarToggleOn,
                         setNavBarData,
                         handleResize,
                         logout
                     };
-                },
-                methods: {
-                    navbarToggleOff(id) {
-                        this.navBarTimeout = setTimeout(() => {
-                            this.navBarToggle[Number(id)] = false;
-                        }, 400);
-                    },
-                    navbarToggleOn(id) {
-                        clearTimeout(this.navBarTimeout);
-                        for(let i in this.navBarToggle){
-                            if(this.navBarToggle.hasOwnProperty(i) && Number(i) !== Number(id)){
-                                this.navBarToggle[Number(i)] = false;
-                            }
-                        }
-                        this.navBarToggle[Number(id)] = true;
-                    }
                 }
             });
             dropDownNavBar.use(Quasar, { config: {} });
+            dropDownNavBar.use(Pinia.createPinia());
             dropDownNavBar.mount('#topNavigation');
         });
     </script>
--- index.php
+++ index.php
@@ -17,46 +17,54 @@ if($GLOBALS['SYMB_UID']){
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo $GLOBALS['DEFAULT_LANG']; ?>">
-<?php
-include_once(__DIR__ . '/config/header-includes.php');
-?>
-<head>
-    <title><?php echo $GLOBALS['DEFAULT_TITLE']; ?> Home</title>
-    <link href="css/base.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" rel="stylesheet" type="text/css" />
-    <link href="css/main.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" rel="stylesheet" type="text/css" />
-    <meta name='keywords' content='' />
-    <?php include_once(__DIR__ . '/config/googleanalytics.php'); ?>
-</head>
-<body>
-<?php
-include(__DIR__ . '/header.php');
-?>
-<div id="innertext">
     <?php
-    if($editor){
-        ?>
-        <fieldset style="width: 300px;">
-            <legend><b>Quick Search</b></legend>
-            <b>Catalog Number</b><br/>
-            <form name="quicksearch" action="collections/editor/occurrenceeditor.php" method="post">
-                <input name="q_catalognumber" type="text" />
-                <input name="collid" type="hidden" value="1" />
-                <input name="occindex" type="hidden" value="0" />
-            </form>
-        </fieldset>
-        <?php
-    }
-    elseif(!$GLOBALS['SYMB_UID']){
-        ?>
-        <a href="profile/index.php">Please login</a>
-        <?php
-    }
+    include_once(__DIR__ . '/config/header-includes.php');
     ?>
-</div>
-
-<?php
-include(__DIR__ . '/footer.php');
-include_once(__DIR__ . '/config/footer-includes.php');
-?>
-</body>
+    <head>
+        <title><?php echo $GLOBALS['DEFAULT_TITLE']; ?> Home</title>
+        <meta name="description" content="Welcome to the <?php echo $GLOBALS['DEFAULT_TITLE']; ?> portal">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/base.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" rel="stylesheet" type="text/css"/>
+        <link href="css/main.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" rel="stylesheet" type="text/css"/>
+        <meta name='keywords' content='' />
+    </head>
+    <body>
+        <?php
+        include(__DIR__ . '/header.php');
+        ?>
+        <div id="mainContainer">
+            <div class="q-pa-md">
+                <?php
+                if($editor){
+                    ?>
+                    <fieldset style="width: 300px;">
+                        <legend><b>Quick Search</b></legend>
+                        <b>Catalog Number</b><br/>
+                        <form name="quicksearch" action="collections/editor/occurrenceeditor.php" method="post">
+                            <input name="q_catalognumber" type="text" />
+                            <input name="collid" type="hidden" value="1" />
+                            <input name="occindex" type="hidden" value="0" />
+                        </form>
+                    </fieldset>
+                    <?php
+                }
+                elseif(!$GLOBALS['SYMB_UID']){
+                    ?>
+                    <a href="profile/index.php">Please login</a>
+                    <?php
+                }
+                ?>
+            </div>
+        </div>
+        <?php
+        include(__DIR__ . '/footer.php');
+        include_once(__DIR__ . '/config/footer-includes.php');
+        ?>
+        <script type="text/javascript">
+            const homePageModule = Vue.createApp();
+            homePageModule.use(Quasar, { config: {} });
+            homePageModule.use(Pinia.createPinia());
+            homePageModule.mount('#mainContainer');
+        </script>
+    </body>
 </html>
