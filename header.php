273cd214894861c3f7f46e7c9b3089bc572adf65
Ben Brandt <benjamin.brandt@nau.edu>
2025-08-11 16:23:02 -0700

    Update

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
-include_once(__DIR__ . '/classes/Sanitizer.php');
+include_once(__DIR__ . '/services/SanitizerService.php');
 ?>
-<div id="mainContainer">
+<div id="appContainer">
     <div id="bannerContainer">
         <div class="bannerTitleContainer">
             <div class="bannerTitle">
@@ -16,7 +16,7 @@ include_once(__DIR__ . '/classes/Sanitizer.php');
     <div id="topNavigation">
         <q-toolbar class="q-pa-md horizontalDropDown">
             <template v-if="windowWidth < 1440">
-                <q-btn class="horizontalDropDownIconButton q-ml-md" flat round dense icon="menu">
+                <q-btn class="horizontalDropDownIconButton q-ml-md" flat round dense icon="menu" aria-label="Menu">
                     <q-menu>
                         <q-list dense>
                             <template v-for="item in navBarData">
@@ -69,38 +69,43 @@ include_once(__DIR__ . '/classes/Sanitizer.php');
             <q-space></q-space>
             <template v-if="userDisplayName">
                 <q-breadcrumbs-el class="header-username-text">Welcome {{ userDisplayName }}!</q-breadcrumbs-el>
-                <q-btn class="horizontalDropDownButton text-capitalize" href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/profile/viewprofile.php" label="My Profile" stretch flat no-wrap></q-btn>
+                <q-btn class="horizontalDropDownButton text-capitalize" :href="clientRoot + '/profile/viewprofile.php'" label="My Profile" stretch flat no-wrap></q-btn>
                 <q-btn class="horizontalDropDownButton text-capitalize" @click="logout();" label="Logout" stretch flat no-wrap></q-btn>
             </template>
             <template v-else>
-                <q-btn class="horizontalDropDownButton text-capitalize" href="<?php echo $GLOBALS['CLIENT_ROOT']. '/profile/index.php?refurl=' .Sanitizer::getCleanedRequestPath(true); ?>" label="Log In" stretch flat no-wrap></q-btn>
-                <q-btn class="horizontalDropDownButton text-capitalize" href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/profile/newprofile.php" label="New Account" stretch flat no-wrap></q-btn>
+                <q-btn class="horizontalDropDownButton text-capitalize" :href="clientRoot + '/profile/index.php?refurl=' + requestPath" label="Log In" stretch flat no-wrap></q-btn>
+                <q-btn class="horizontalDropDownButton text-capitalize" :href="clientRoot + '/profile/newprofile.php'" label="New Account" stretch flat no-wrap></q-btn>
             </template>
-            <q-btn class="horizontalDropDownButton text-capitalize" href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/sitemap.php" label="Sitemap" stretch flat no-wrap></q-btn>
+            <q-btn class="horizontalDropDownButton text-capitalize" :href="clientRoot + '/sitemap.php'" label="Sitemap" stretch flat no-wrap></q-btn>
         </q-toolbar>
     </div>
-    <script>
+    <script type="text/javascript">
+        const REQUEST_PATH = "<?php echo SanitizerService::getCleanedRequestPath(true); ?>";
         document.addEventListener("DOMContentLoaded", () => {
             const dropDownNavBar = Vue.createApp({
                 setup() {
+                    const store = useBaseStore();
+                    const storeRefs = Pinia.storeToRefs(store);
+                    const clientRoot = store.getClientRoot;
                     const navBarData = Vue.ref([
                         {url: CLIENT_ROOT + '/index.php', label: 'Home'},
                         {url: CLIENT_ROOT + '/misc/project.php', label: 'The Project'},
                         {label: 'Trees'},
-                        {url: CLIENT_ROOT + '/collections/index.php', label: 'Specimen Search'},
+                        {url: CLIENT_ROOT + '/collections/list.php', label: 'Specimen Search'},
                         {url: CLIENT_ROOT + '/imagelib/search.php', label: 'Image Search'},
                         {url: CLIENT_ROOT + '/spatial/index.php', label: 'Map Search', newTab: true},
                         {
                             label: 'Interactive Tools',
                             subItems: [
-                                {url: CLIENT_ROOT + '/checklists/dynamicmap.php?interface=checklist', label: 'Dynamic Checklist'},
-                                {url: CLIENT_ROOT + '/checklists/dynamicmap.php?interface=key', label: 'Dynamic Key'}
+                                {url: CLIENT_ROOT + '/checklists/checklist.php', label: 'Dynamic Checklist'},
+                                {url: CLIENT_ROOT + '/ident/key.php', label: 'Dynamic Key'}
                             ]
                         }
                     ]);
                     let navBarTimeout = null;
                     const navBarToggle = Vue.ref({});
-                    const userDisplayName = USER_DISPLAY_NAME;
+                    const requestPath = REQUEST_PATH;
+                    const userDisplayName = storeRefs.getUserDisplayName;
                     const windowWidth = Vue.ref(0);
 
                     function  handleResize() {
@@ -111,8 +116,24 @@ include_once(__DIR__ . '/classes/Sanitizer.php');
                         const url = profileApiUrl + '?action=logout';
                         fetch(url)
                             .then(() => {
-                                window.location.href = CLIENT_ROOT + '/index.php';
+                                window.location.href = clientRoot + '/index.php';
                             })
+                    }
+
+                    function navbarToggleOff(id) {
+                        this.navBarTimeout = setTimeout(() => {
+                            this.navBarToggle[Number(id)] = false;
+                        }, 400);
+                    }
+
+                    function navbarToggleOn(id) {
+                        clearTimeout(this.navBarTimeout);
+                        for(let i in this.navBarToggle){
+                            if(this.navBarToggle.hasOwnProperty(i) && Number(i) !== Number(id)){
+                                this.navBarToggle[Number(i)] = false;
+                            }
+                        }
+                        this.navBarToggle[Number(id)] = true;
                     }
 
                     function setNavBarData() {
@@ -131,34 +152,23 @@ include_once(__DIR__ . '/classes/Sanitizer.php');
                     });
 
                     return {
+                        clientRoot,
                         navBarData,
                         navBarToggle,
                         navBarTimeout,
+                        requestPath,
                         userDisplayName,
                         windowWidth,
+                        navbarToggleOff,
+                        navbarToggleOn,
                         setNavBarData,
                         handleResize,
                         logout
                     };
-                },
-                methods: {
-                    navbarToggleOff(id) {
-                        this.navBarTimeout = setTimeout(() => {
-                            this.navBarToggle[Number(id)] = false;
-                        }, 400);
-                    },
-                    navbarToggleOn(id) {
-                        clearTimeout(this.navBarTimeout);
-                        for(let i in this.navBarToggle){
-                            if(this.navBarToggle.hasOwnProperty(i) && Number(i) !== Number(id)){
-                                this.navBarToggle[Number(i)] = false;
-                            }
-                        }
-                        this.navBarToggle[Number(id)] = true;
-                    }
                 }
             });
             dropDownNavBar.use(Quasar, { config: {} });
+            dropDownNavBar.use(Pinia.createPinia());
             dropDownNavBar.mount('#topNavigation');
         });
     </script>
--- index.php
+++ index.php
@@ -9,86 +9,78 @@ header('Content-Type: text/html; charset=UTF-8' );
     ?>
     <head>
         <title><?php echo $GLOBALS['DEFAULT_TITLE']; ?> Home</title>
-        <link href="css/base.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" type="text/css" rel="stylesheet" />
-        <link href="css/main.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" type="text/css" rel="stylesheet" />
-        <link href="css/external/jquery-ui.css" type="text/css" rel="Stylesheet" />
-        <script src="js/external/jquery.js" type="text/javascript"></script>
-        <script src="js/external/jquery-ui.js" type="text/javascript"></script>
-        <?php include_once('config/googleanalytics.php'); ?>
+        <meta name="description" content="Welcome to the <?php echo $GLOBALS['DEFAULT_TITLE']; ?> portal">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/base.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" rel="stylesheet" type="text/css"/>
+        <link href="css/main.css?ver=<?php echo $GLOBALS['CSS_VERSION']; ?>" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <?php
         include(__DIR__ . '/header.php');
         ?>
-        <div id="innertext">
-            <div style="float:right;width:380px;">
-                <?php
-                $oodID = 1;
-                $ootdGameChecklist = 1;
-                $ootdGameTitle = 'Plant of the Day ';
-                $ootdGameType = 'plant';
-
-                include_once(__DIR__ . '/classes/GamesManager.php');
-                $gameManager = new GamesManager();
-                $gameInfo = $gameManager->setOOTD($oodID,$ootdGameChecklist);
-                ?>
-                <div style="float:right;margin-top:15px;margin-right:10px;margin-bottom:15px;width:350px;text-align:center;">
-                    <div style="font-size:130%;font-weight:bold;">
-                        <?php echo $ootdGameTitle; ?>
-                    </div>
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/games/ootd/index.php?oodid=<?php echo $oodID.'&cl='.$ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
-                        <img src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/temp/ootd/<?php echo $oodID; ?>_organism300_1.jpg" style="width:350px;border:0;" />
-                    </a><br/>
-                    <b>What is this <?php echo $ootdGameType; ?>?</b><br/>
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/games/ootd/index.php?oodid=<?php echo $oodID.'&cl='.$ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
-                        Click here to test your knowledge
-                    </a>
-                </div>
-            </div>
-            <div style="padding: 0 15px;margin-top:15px;">
-                The genus <i>Lomatium</i> is the largest of 18 genera of Apiaceae from primarily Western North America that
-                form a large clade of over 200 species.  This website is the result of an NSF funded project to create a
-                phylogenetic hypothesis and a new classification for this previously poorly understood group.  This electronic
-                monograph contains descriptions, synonymy, protologues, distribution maps, specimen records, and photographs
-                for all taxa in this group.  Phylogenetic trees and GenBank records of DNA accessions used in our study are
-                also included.
-            </div>
-            <div style="margin-top:15px;padding: 0 10px;">
-                The 18 genera covered in this treatment are:
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Aletes" ><i>Aletes</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Cymopterus" ><i>Cymopterus</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Eurytaenia" ><i>Eurytaenia</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Harbouria" ><i>Harbouria</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Lomatium" ><i>Lomatium</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Musineon" ><i>Musineon</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Neoparrya" ><i>Neoparrya</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Oreonana" ><i>Oreonana</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Oreoxis" ><i>Oreoxis</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Podistera" ><i>Podistera</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Polytaenia" ><i>Polytaenia</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Pseudocymopterus" ><i>Pseudocymopterus</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Shoshonea" ><i>Shoshonea</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Taenidia" ><i>Taenidia</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Tauschia" ><i>Tauschia</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Thaspium" ><i>Thaspium</i></a>,
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Vesper" ><i>Vesper</i></a>, and
-                <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Zizia" ><i>Zizia</i></a>.
-            </div>
-            <div style="margin-top:25px;display:flex;justify-content:center;">
-                <div style="width:450px;">
+        <div id="mainContainer">
+            <div class="q-pa-md">
+                <div style="float:right;width:380px;">
                     <?php
-                    $searchText = '';
-                    $buttonText = 'Search';
-                    include_once(__DIR__ . '/classes/PluginsManager.php');
-                    $pluginManager = new PluginsManager();
-                    $pluginManager->setQuickSearchShowSelector(true);
-                    $quicksearch = $pluginManager->createQuickSearch($buttonText,$searchText);
-                    echo $quicksearch;
+                    $oodID = 1;
+                    $ootdGameChecklist = 1;
+                    $ootdGameTitle = 'Plant of the Day ';
+                    $ootdGameType = 'plant';
+
+                    include_once(__DIR__ . '/classes/GamesManager.php');
+                    $gameManager = new GamesManager();
+                    $gameInfo = $gameManager->setOOTD($oodID,$ootdGameChecklist);
                     ?>
+                    <div style="float:right;margin-top:15px;margin-right:10px;margin-bottom:15px;width:350px;text-align:center;">
+                        <div style="font-size:130%;font-weight:bold;">
+                            <?php echo $ootdGameTitle; ?>
+                        </div>
+                        <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/games/ootd/index.php?oodid=<?php echo $oodID.'&cl='.$ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
+                            <img src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/temp/ootd/<?php echo $oodID; ?>_organism300_1.jpg" style="width:350px;border:0;" />
+                        </a><br/>
+                        <b>What is this <?php echo $ootdGameType; ?>?</b><br/>
+                        <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/games/ootd/index.php?oodid=<?php echo $oodID.'&cl='.$ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
+                            Click here to test your knowledge
+                        </a>
+                    </div>
+                </div>
+                <div style="padding: 0 15px;margin-top:15px;">
+                    The genus <i>Lomatium</i> is the largest of 18 genera of Apiaceae from primarily Western North America that
+                    form a large clade of over 200 species.  This website is the result of an NSF funded project to create a
+                    phylogenetic hypothesis and a new classification for this previously poorly understood group.  This electronic
+                    monograph contains descriptions, synonymy, protologues, distribution maps, specimen records, and photographs
+                    for all taxa in this group.  Phylogenetic trees and GenBank records of DNA accessions used in our study are
+                    also included.
+                </div>
+                <div style="margin-top:15px;padding: 0 10px;">
+                    The 18 genera covered in this treatment are:
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Aletes" ><i>Aletes</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Cymopterus" ><i>Cymopterus</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Eurytaenia" ><i>Eurytaenia</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Harbouria" ><i>Harbouria</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Lomatium" ><i>Lomatium</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Musineon" ><i>Musineon</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Neoparrya" ><i>Neoparrya</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Oreonana" ><i>Oreonana</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Oreoxis" ><i>Oreoxis</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Podistera" ><i>Podistera</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Polytaenia" ><i>Polytaenia</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Pseudocymopterus" ><i>Pseudocymopterus</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Shoshonea" ><i>Shoshonea</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Taenidia" ><i>Taenidia</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Tauschia" ><i>Tauschia</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Thaspium" ><i>Thaspium</i></a>,
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Vesper" ><i>Vesper</i></a>, and
+                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Zizia" ><i>Zizia</i></a>.
+                </div>
+                <div style="margin-top:25px;display:flex;justify-content:center;">
+                    <div>
+                        <taxa-quick-search default-taxon-type="scientific"></taxa-quick-search>
+                    </div>
                 </div>
             </div>
         </div>
-        <div style="width:100%;clear:both;"></div>
+        <div style="clear:both;"></div>
         <div class="footer">
             <p style="text-align:center;">© <a href="http://herbarium.wisc.edu/" title="Wisconsin State Herbarium home">Wisconsin State Herbarium, UW-Madison</a>
                 <br>
@@ -101,5 +93,15 @@ header('Content-Type: text/html; charset=UTF-8' );
         <?php
         include_once(__DIR__ . '/config/footer-includes.php');
         ?>
+        <script type="text/javascript">
+            const homePageModule = Vue.createApp({
+                components: {
+                    'taxa-quick-search': taxaQuickSearch
+                }
+            });
+            homePageModule.use(Quasar, { config: {} });
+            homePageModule.use(Pinia.createPinia());
+            homePageModule.mount('#mainContainer');
+        </script>
     </body>
 </html>
