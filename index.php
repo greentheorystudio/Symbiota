70921e5b748feb5fffaa4a5e7c57ae2b9d506ca5
Ben Brandt <benjamin.brandt@nau.edu>
2025-08-19 14:19:50 -0700

    Update

--- index.php
+++ index.php
@@ -20,58 +20,41 @@ header('Content-Type: text/html; charset=UTF-8' );
         ?>
         <div id="mainContainer">
             <div class="q-pa-md">
-                <div style="float:right;width:380px;">
-                    <?php
-                    $oodID = 1;
-                    $ootdGameChecklist = 1;
-                    $ootdGameTitle = 'Plant of the Day ';
-                    $ootdGameType = 'plant';
-
-                    include_once(__DIR__ . '/classes/GamesManager.php');
-                    $gameManager = new GamesManager();
-                    $gameInfo = $gameManager->setOOTD($oodID,$ootdGameChecklist);
-                    ?>
-                    <div style="float:right;margin-top:15px;margin-right:10px;margin-bottom:15px;width:350px;text-align:center;">
-                        <div style="font-size:130%;font-weight:bold;">
-                            <?php echo $ootdGameTitle; ?>
+                <div class="q-mb-md row q-col-gutter-md">
+                    <div class="col-12 col-sm-6 col-md-7">
+                        <div style="padding: 0 15px;margin-top:15px;">
+                            The genus <i>Lomatium</i> is the largest of 18 genera of Apiaceae from primarily Western North America that
+                            form a large clade of over 200 species.  This website is the result of an NSF funded project to create a
+                            phylogenetic hypothesis and a new classification for this previously poorly understood group.  This electronic
+                            monograph contains descriptions, synonymy, protologues, distribution maps, specimen records, and photographs
+                            for all taxa in this group.  Phylogenetic trees and GenBank records of DNA accessions used in our study are
+                            also included.
                         </div>
-                        <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/games/ootd/index.php?oodid=<?php echo $oodID.'&cl='.$ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
-                            <img src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/temp/ootd/<?php echo $oodID; ?>_organism300_1.jpg" style="width:350px;border:0;" />
-                        </a><br/>
-                        <b>What is this <?php echo $ootdGameType; ?>?</b><br/>
-                        <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/games/ootd/index.php?oodid=<?php echo $oodID.'&cl='.$ootdGameChecklist.'&title='.$ootdGameTitle.'&type='.$ootdGameType; ?>">
-                            Click here to test your knowledge
-                        </a>
+                        <div style="margin-top:15px;padding: 0 10px;">
+                            The 18 genera covered in this treatment are:
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Aletes" ><i>Aletes</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Cymopterus" ><i>Cymopterus</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Eurytaenia" ><i>Eurytaenia</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Harbouria" ><i>Harbouria</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Lomatium" ><i>Lomatium</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Musineon" ><i>Musineon</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Neoparrya" ><i>Neoparrya</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Oreonana" ><i>Oreonana</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Oreoxis" ><i>Oreoxis</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Podistera" ><i>Podistera</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Polytaenia" ><i>Polytaenia</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Pseudocymopterus" ><i>Pseudocymopterus</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Shoshonea" ><i>Shoshonea</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Taenidia" ><i>Taenidia</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Tauschia" ><i>Tauschia</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Thaspium" ><i>Thaspium</i></a>,
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Vesper" ><i>Vesper</i></a>, and
+                            <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Zizia" ><i>Zizia</i></a>.
+                        </div>
                     </div>
-                </div>
-                <div style="padding: 0 15px;margin-top:15px;">
-                    The genus <i>Lomatium</i> is the largest of 18 genera of Apiaceae from primarily Western North America that
-                    form a large clade of over 200 species.  This website is the result of an NSF funded project to create a
-                    phylogenetic hypothesis and a new classification for this previously poorly understood group.  This electronic
-                    monograph contains descriptions, synonymy, protologues, distribution maps, specimen records, and photographs
-                    for all taxa in this group.  Phylogenetic trees and GenBank records of DNA accessions used in our study are
-                    also included.
-                </div>
-                <div style="margin-top:15px;padding: 0 10px;">
-                    The 18 genera covered in this treatment are:
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Aletes" ><i>Aletes</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Cymopterus" ><i>Cymopterus</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Eurytaenia" ><i>Eurytaenia</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Harbouria" ><i>Harbouria</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Lomatium" ><i>Lomatium</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Musineon" ><i>Musineon</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Neoparrya" ><i>Neoparrya</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Oreonana" ><i>Oreonana</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Oreoxis" ><i>Oreoxis</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Podistera" ><i>Podistera</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Polytaenia" ><i>Polytaenia</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Pseudocymopterus" ><i>Pseudocymopterus</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Shoshonea" ><i>Shoshonea</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Taenidia" ><i>Taenidia</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Tauschia" ><i>Tauschia</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Thaspium" ><i>Thaspium</i></a>,
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Vesper" ><i>Vesper</i></a>, and
-                    <a href="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/taxa/index.php?taxon=Zizia" ><i>Zizia</i></a>.
+                    <div class="col-12 col-sm-6 col-md-5 row justify-center">
+                        <organism-of-the-day checklist-id="1" title="Plant of the Day" type="plant"></organism-of-the-day>
+                    </div>
                 </div>
                 <div style="margin-top:25px;display:flex;justify-content:center;">
                     <div>
@@ -93,9 +76,17 @@ header('Content-Type: text/html; charset=UTF-8' );
         <?php
         include_once(__DIR__ . '/config/footer-includes.php');
         ?>
+        <script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/stores/taxa-vernacular.js?ver=<?php echo $GLOBALS['JS_VERSION']; ?>" type="text/javascript"></script>
+        <script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/stores/project.js?ver=<?php echo $GLOBALS['JS_VERSION']; ?>" type="text/javascript"></script>
+        <script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/stores/checklist-taxa.js?ver=<?php echo $GLOBALS['JS_VERSION']; ?>" type="text/javascript"></script>
+        <script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/stores/checklist.js?ver=<?php echo $GLOBALS['JS_VERSION']; ?>" type="text/javascript"></script>
+        <script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/components/media/imageCarousel.js?ver=<?php echo $GLOBALS['JS_VERSION']; ?>" type="text/javascript"></script>
+        <script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/components/input-elements/singleScientificCommonNameAutoComplete.js?ver=<?php echo $GLOBALS['JS_VERSION']; ?>" type="text/javascript"></script>
+        <script src="<?php echo $GLOBALS['CLIENT_ROOT']; ?>/components/games/organismOfTheDay.js?ver=<?php echo $GLOBALS['JS_VERSION']; ?>" type="text/javascript"></script>
         <script type="text/javascript">
             const homePageModule = Vue.createApp({
                 components: {
+                    'organism-of-the-day': organismOfTheDay,
                     'taxa-quick-search': taxaQuickSearch
                 }
             });
