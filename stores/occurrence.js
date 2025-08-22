609a4ddda6cbd89d1a851c99a3faec943c11618c
Ben Brandt <benjamin.brandt@nau.edu>
2025-08-22 10:52:23 -0700

    WIP

--- stores/occurrence.js
+++ stores/occurrence.js
@@ -888,7 +888,7 @@ const useOccurrenceStore = Pinia.defineStore('occurrence', {
         setCurrentCollectingEventRecord(eventid) {
             this.collectingEventStore.setCurrentCollectingEventRecord(eventid, this.occurrenceEntryFormat, this.getCollectionData['defaultrepcount'], this.getEventMofDataFields, () => {
                 this.setCurrentOccurrenceRecord(this.occId);
-                this.updateOccurrenceEditData('eventid', this.getCollectingEventID.toString());
+                this.updateOccurrenceEditData('eventid', (Number(this.getCollectingEventID) > 0 ? this.getCollectingEventID.toString() : null));
             });
         },
         setCurrentDeterminationRecord(detid) {
@@ -899,7 +899,7 @@ const useOccurrenceStore = Pinia.defineStore('occurrence', {
         },
         setCurrentLocationRecord(locationid) {
             this.locationStore.setCurrentLocationRecord(locationid, this.getCollId, () => {
-                this.updateOccurrenceEditData('locationid', this.getLocationID.toString());
+                this.updateOccurrenceEditData('locationid', (Number(this.getLocationID) > 0 ? this.getLocationID.toString() : null));
                 this.collectingEventStore.getLocationCollectingEvents(this.getCollId, locationid);
             });
         },
