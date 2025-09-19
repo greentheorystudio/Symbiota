72a90412bc52b2c7bb2f9dc1fbcaf7ccd1b40060
Ben Brandt <benjamin.brandt@nau.edu>
2025-09-19 16:01:34 -0700

    WIP

--- models/Configurations.php
+++ models/Configurations.php
@@ -359,7 +359,7 @@ class Configurations{
             }
         }
         $GLOBALS['CSS_VERSION'] = '20250203';
-        $GLOBALS['JS_VERSION'] = '202505201';
+        $GLOBALS['JS_VERSION'] = '2025052011';
         $GLOBALS['PARAMS_ARR'] = array();
         $GLOBALS['USER_RIGHTS'] = array();
         $this->validateGlobalArr();
