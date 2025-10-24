7dd925b01a58b8143d9b7e473bf0d9ec7ea8bced
Ben Brandt <benjamin.brandt@nau.edu>
2025-10-24 10:42:05 -0600

    WIP

--- models/Configurations.php
+++ models/Configurations.php
@@ -139,30 +139,6 @@ class Configurations{
         $this->conn->close();
     }
 
-    public function getConfigurationsArr(): array
-    {
-        $retArr = array();
-        $retArr['core'] = array();
-        $retArr['additional'] = array();
-        $sql = 'SELECT configurationname, configurationvalue FROM configurations ';
-        $rs = $this->conn->query($sql);
-        while($r = $rs->fetch_object()){
-            $value = $r->configurationvalue;
-            if(strpos($r->configurationname, 'PASSWORD') !== false || strpos($r->configurationname, 'USERNAME') !== false){
-                $value = EncryptionService::decrypt($value);
-            }
-            $retArr[$r->configurationname] = $value;
-            if(in_array($r->configurationname, $this->coreConfigurations, true)){
-                $retArr['core'][$r->configurationname] = $value;
-            }
-            else{
-                $retArr['additional'][$r->configurationname] = $value;
-            }
-        }
-        $rs->free();
-        return $retArr;
-    }
-
     public function addConfiguration($name, $value): int
     {
         $returnVal = 0;
