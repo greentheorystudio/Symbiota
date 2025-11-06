57999e4d069a6e4ea5d506e0456db01f962dc4a2
Ben Brandt <benjamin.brandt@nau.edu>
2025-11-06 12:08:43 -0700

    Several updates

--- taxa/profile-custom.php
+++ taxa/profile-custom.php
@@ -160,12 +160,14 @@
                 taxaStore.setTaxa(taxonValue, (tid) => {
                     hideWorking();
                     if(Number(tid) > 0){
-                        taxaStore.setTaxaDescriptionData();
+                        taxaStore.setTaxonVernacularArr(taxon.value['tidaccepted']);
+                        taxaStore.setTaxonMapArr(taxon.value['tidaccepted']);
+                        taxaStore.setTaxonDescriptionData(taxon.value['tidaccepted']);
                         if(subtaxaArr.value.length > 0){
                             taxaStore.setSubtaxaImageData();
                         }
-                        taxaStore.setTaxaImageArr();
-                        taxaStore.setTaxaMediaArr();
+                        taxaStore.setTaxaImageArr(taxon.value['tidaccepted']);
+                        taxaStore.setTaxaMediaArr(taxon.value['tidaccepted']);
                     }
                     else if(taxonValue.value && taxonValue.value !== ''){
                         taxaStore.setFuzzyMatches();
