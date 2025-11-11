b37bd02c3f218d94a40058b5b2072be55117b910
Ben Brandt <benjamin.brandt@nau.edu>
2025-11-11 11:42:19 -0700

    WIP

--- components/taxonomy/newTaxonEditorPopup.js
+++ components/taxonomy/newTaxonEditorPopup.js
@@ -17,7 +17,9 @@ const newTaxonEditorPopup = {
                     <div :style="contentStyle" class="overflow-auto">
                         <div class="q-pa-md column q-col-gutter-sm">
                             <div class="row justify-between">
-                                <div></div>
+                                <div>
+                                    <span v-if="errorMessage" class="text-subtitle1 text-bold text-red">{{ errorMessage }}</span>
+                                </div>
                                 <div class="row justify-end q-gutter-xs">
                                     <q-btn color="secondary" @click="addTaxon();" label="Add Taxon" :disabled="!taxonValid" tabindex="0" />
                                 </div>
@@ -38,15 +40,29 @@ const newTaxonEditorPopup = {
 
         const contentRef = Vue.ref(null);
         const contentStyle = Vue.ref(null);
+        const errorMessage = Vue.computed(() => {
+            let errorMessage = null;
+            if(taxonData.value['sciname'] && taxonData.value['sciname'].length > 0){
+                if(Number(taxonData.value['rankid']) !== 10 && Number(taxonData.value['parenttid']) === 0){
+                    errorMessage = 'Parent taxon required.';
+                }
+                else if(!uniqueTaxon.value){
+                    errorMessage = 'Taxon already exists in the thesaurus.';
+                }
+            }
+            return errorMessage;
+        });
         const taxonData = Vue.ref(null);
         const taxonValid = Vue.computed(() => {
             return (
+                uniqueTaxon.value &&
                 (taxonData.value['sciname'] && taxonData.value['sciname'].length > 0) &&
                 (Number(taxonData.value['rankid']) === 10 || Number(taxonData.value['kingdomid'])) > 0 &&
                 Number(taxonData.value['rankid']) > 0 &&
                 (Number(taxonData.value['rankid']) === 10 || Number(taxonData.value['parenttid']) > 0)
             );
         });
+        const uniqueTaxon = Vue.ref(false);
 
         Vue.watch(contentRef, () => {
             setContentStyle();
@@ -55,9 +71,9 @@ const newTaxonEditorPopup = {
         function addTaxon() {
             taxaStore.createTaxonRecord(taxonData.value, (newTaxonId) => {
                 if(newTaxonId > 0){
-                    showNotification('positive','Taxon added successfully.');
                     context.emit('taxon:created', newTaxonId);
                     context.emit('close:popup');
+                    showNotification('positive','Taxon added successfully.');
                 }
                 else{
                     showNotification('negative', 'There was an error adding the new taxon.');
@@ -78,6 +94,34 @@ const newTaxonEditorPopup = {
 
         function updateTaxonData(key, value) {
             taxonData.value[key] = value;
+            if(key === 'sciname' || key === 'kingdomid'){
+                validateUniqueTaxon();
+            }
+        }
+
+        function validateUniqueTaxon() {
+            uniqueTaxon.value = false;
+            if(Number(taxonData.value['rankid']) === 10){
+                uniqueTaxon.value = true;
+            }
+            else if(taxonData.value['sciname'] && taxonData.value['sciname'].length > 0 && Number(taxonData.value['kingdomid']) > 0){
+                const formData = new FormData();
+                formData.append('action', 'getTaxonFromSciname');
+                formData.append('kingdomid', taxonData.value['kingdomid'].toString());
+                formData.append('sciname', taxonData.value['sciname']);
+                fetch(taxaApiUrl, {
+                    method: 'POST',
+                    body: formData
+                })
+                .then((response) => {
+                    return response.ok ? response.json() : null;
+                })
+                .then((data) => {
+                    if(!data.hasOwnProperty('tid') || Number(data['tid']) === 0){
+                        uniqueTaxon.value = true;
+                    }
+                });
+            }
         }
 
         Vue.onMounted(() => {
@@ -89,6 +133,7 @@ const newTaxonEditorPopup = {
         return {
             contentRef,
             contentStyle,
+            errorMessage,
             taxonData,
             taxonValid,
             addTaxon,
