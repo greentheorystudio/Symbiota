cbedb75cb5ef79e5ceed7a7b92b35dd2be693acb
Ben Brandt <benjamin.brandt@nau.edu>
2025-12-16 09:42:40 -0700

    WIP

--- components/occurrences/mofDataFieldRowGroup.js
+++ components/occurrences/mofDataFieldRowGroup.js
@@ -27,25 +27,29 @@ const mofDataFieldRowGroup = {
     },
     template: `
         <template v-if="expansion">
-            <q-expansion-item class="shadow-1 overflow-hidden expansion-element" :label="label" header-class="bg-grey-3 text-grey-8 text-h6 text-weight-bolder q-pl-md" expand-icon-class="text-bold">
-                <div class="q-pa-sm column q-col-gutter-sm">
-                    <template v-for="row in rows">
-                        <mof-data-field-row :editor="editor" :configured-data="configuredData" :configured-data-fields="configuredDataFields" :fields="row.fields" @update:configured-edit-data="updateConfiguredEditData"></mof-data-field-row>
-                    </template>
-                </div>
-            </q-expansion-item>
+            <div>
+                <q-expansion-item class="shadow-1 overflow-hidden expansion-element" :label="label" header-class="bg-grey-3 text-grey-8 text-h6 text-weight-bolder q-pl-md" expand-icon-class="text-bold">
+                    <div class="q-pa-sm column q-col-gutter-sm">
+                        <template v-for="row in rows">
+                            <mof-data-field-row :editor="editor" :configured-data="configuredData" :configured-data-fields="configuredDataFields" :fields="row.fields" @update:configured-edit-data="updateConfiguredEditData"></mof-data-field-row>
+                        </template>
+                    </div>
+                </q-expansion-item>
+            </div>
         </template>
         <template v-else>
-            <q-card flat bordered>
-                <q-card-section v-if="rows.length > 0" class="q-pa-sm column q-col-gutter-sm">
-                    <div v-if="label" class="text-grey-8 text-h6 text-weight-bolder q-pl-md">
-                        {{ label }}
-                    </div>
-                    <template v-for="row in rows">
-                        <mof-data-field-row :editor="editor" :configured-data="configuredData" :configured-data-fields="configuredDataFields" :fields="row.fields" @update:configured-edit-data="updateConfiguredEditData"></mof-data-field-row>
-                    </template>
-                </q-card-section>
-            </q-card>
+            <div>
+                <q-card flat bordered>
+                    <q-card-section v-if="rows.length > 0" class="q-pa-sm column q-col-gutter-sm">
+                        <div v-if="label" class="text-grey-8 text-h6 text-weight-bolder q-pl-md">
+                            {{ label }}
+                        </div>
+                        <template v-for="row in rows">
+                            <mof-data-field-row :editor="editor" :configured-data="configuredData" :configured-data-fields="configuredDataFields" :fields="row.fields" @update:configured-edit-data="updateConfiguredEditData"></mof-data-field-row>
+                        </template>
+                    </q-card-section>
+                </q-card>
+            </div>
         </template>
     `,
     components: {
