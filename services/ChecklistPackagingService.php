81b527222b268356dbf336886485033ca9fdaba0
Ben Brandt <benjamin.brandt@nau.edu>
2026-01-02 10:08:38 -0700

    WIP

--- services/ChecklistPackagingService.php
+++ services/ChecklistPackagingService.php
@@ -507,7 +507,18 @@ class ChecklistPackagingService {
     {
         $fullArchivePath = $GLOBALS['SERVER_ROOT'] . '/content/checklist/cl-' . $clid . '.zip';
         if(FileSystemService::fileExists($fullArchivePath)){
-            (new DataDownloadService)->streamDownload('application/zip', $fullArchivePath, true);
+            if(ob_get_level()){
+                ob_end_clean();
+            }
+            header('Content-Type: application/zip');
+            //header('Content-Disposition: attachment; filename=' . basename($fullArchivePath));
+            //header('Content-Transfer-Encoding: binary');
+            //header('Expires: 0');
+            //header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            //header('Pragma: public');
+            header('Content-Length: ' . filesize($fullArchivePath));
+            readfile($fullArchivePath);
+            flush();
         }
     }
 
