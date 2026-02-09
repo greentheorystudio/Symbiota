59964dd33fe266ff01251951b5a8e03644539524
ericaloomis <124950222+ericaloomis@users.noreply.github.com>
2026-02-09 16:20:34 -0500

    WIP

--- models/Configurations.php
+++ models/Configurations.php
@@ -368,7 +368,7 @@ class Configurations{
             }
         }
         $GLOBALS['CSS_VERSION'] = '20251115';
-        $GLOBALS['JS_VERSION'] = '2026010641111111111111';
+        $GLOBALS['JS_VERSION'] = '20260106411111111';
         $GLOBALS['PARAMS_ARR'] = array();
         $GLOBALS['USER_RIGHTS'] = array();
         $this->validateGlobalArr();
