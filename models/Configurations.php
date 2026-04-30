ac970366d5a49afefe0016178ad239bfba78dd73
Ben Brandt <benjamin.brandt@nau.edu>
2026-04-30 12:19:50 -0700

    WIP

--- models/Configurations.php
+++ models/Configurations.php
@@ -368,7 +368,7 @@ class Configurations{
                 $this->initializeImportConfigurations();
             }
         }
-        $GLOBALS['CSS_VERSION'] = '20260215139';
+        $GLOBALS['CSS_VERSION'] = '20260301';
         $GLOBALS['JS_VERSION'] = '2026040122223333';
         $GLOBALS['PARAMS_ARR'] = array();
         $GLOBALS['USER_RIGHTS'] = array();
