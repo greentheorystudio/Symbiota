2f40abe071848599a532d67247d6486de22ee688
Ben Brandt <benjamin.brandt@nau.edu>
2026-08-19 11:53:58 -0700

    WIP

--- collections/misc/collprofiles.php
+++ collections/misc/collprofiles.php
@@ -227,7 +227,7 @@ $collid = (array_key_exists('collid',$_REQUEST) ? (int)$_REQUEST['collid'] : 0);
                             <q-card v-if="collection" flat bordered>
                                 <q-card-section horizontal class="q-pa-md">
                                     <q-card-section v-if="collection.icon" class="col-3 column flex flex-center">
-                                        <q-img class="rounded-borders coll-icon-image" width="75%" :src="collection.icon"></q-img>
+                                        <q-img class="rounded-borders coll-icon-image" width="75%" :src="(collection.icon.startsWith('/') ? (clientRoot + collection.icon) : collection.icon)"></q-img>
                                     </q-card-section>
                                     <q-card-section class="column">
                                         <div class="text-h6 text-bold">
