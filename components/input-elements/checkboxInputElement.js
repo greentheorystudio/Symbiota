8fc15487dedaa9e5faa2febf31b8cb8c057951f2
Ben Brandt <benjamin.brandt@nau.edu>
2026-09-14 17:14:29 -0700

    WIP

--- components/input-elements/checkboxInputElement.js
+++ components/input-elements/checkboxInputElement.js
@@ -23,7 +23,7 @@ const checkboxInputElement = {
     },
     template: `
         <div class="row inline q-gutter-x-xs">
-            <div>
+            <div class="text-subtitle1">
                 <q-checkbox v-model="checkboxValue" :label="label" :disable="disabled" @update:model-value="processValueChange" :tabindex="tabindex" dense></q-checkbox>
             </div>
             <div v-if="!disabled && definition" class="self-center">
